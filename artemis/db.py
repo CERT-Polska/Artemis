38cdb7cc491e8a4aa0610f5d4bd8d015f52141da
Krzysztof Zając <krzysztof.zajac@cert.pl>
2022-12-02 11:52:42 +0100

    WIP: the possibility to make a decision about a finding

--- artemis/db.py
+++ artemis/db.py
@@ -1,23 +1,63 @@
+import dataclasses
 import json
+from enum import Enum
 from typing import Any, Dict, List, Optional, cast
 
 from karton.core import Task
 from pydantic import BaseModel
 from pymongo import MongoClient
 
-from artemis.binds import TaskStatus
+from artemis.binds import TaskStatus, TaskType
 from artemis.config import Config
+
+
+class ManualDecisionType(str, Enum):
+    INTERESTING = "interesting"
+    NOT_INTERESTING = "not_interesting"
+
+
+@dataclasses.dataclass
+class TaskResultManualDecision:
+    target_string: Optional[str]
+    message: str
+    decision_type: ManualDecisionType
+    operator_comment: str
+
+
+def get_task_target_as_string(task: Task) -> str:
+    result = None
+    if task.headers["type"] == TaskType.NEW:
+        result = task.payload.get("data", None)
+    elif task.headers["type"] == TaskType.IP:
+        result = task.payload.get("ip", None)
+    elif task.headers["type"] == TaskType.DOMAIN:
+        result = task.payload.get("domain", None)
+    elif task.headers["type"] == TaskType.WEBAPP:
+        result = task.payload.get("url", None)
+    elif task.headers["type"] == TaskType.SERVICE:
+        if "host" in task.payload and "port" in task.payload:
+            result = task.payload["host"] + ":" + task.payload["port"]
+
+    if not result:
+        result = task.headers["type"] + ": " + task.uid
+
+    assert isinstance(result, str)
+    return result
 
 
 class DB:
     def __init__(self) -> None:
         self.client = MongoClient(Config.DB_CONN_STR)
         self.analysis = self.client.artemis.analysis
+        self.task_result_manual_decisions = self.client.artemis.task_result_manual_decisions
         self.scheduled_tasks = self.client.artemis.scheduled_tasks
         self.task_results = self.client.artemis.task_results
 
     def list_analysis(self) -> List[Dict[str, Any]]:
         return cast(List[Dict[str, Any]], list(self.analysis.find()))
+
+    def add_task_result_manual_decision(self, decision: TaskResultManualDecision) -> None:
+        self.task_result_manual_decisions.insert_one(decision.asdict())
 
     def create_analysis(self, analysis: Task) -> None:
         created_analysis = self.task_to_dict(analysis)
@@ -35,6 +75,7 @@ class DB:
 
         created_task_result["_id"] = created_task_result["uid"]
         created_task_result["status"] = status
+        created_task_result["target_string"] = get_task_target_as_string(task)
         created_task_result["status_reason"] = status_reason
 
         if isinstance(data, BaseModel):
@@ -54,15 +95,34 @@ class DB:
 
     def get_task_results_by_analysis_id(self, analysis_id: str, status: Optional[str] = None) -> List[Dict[str, Any]]:
         if status:
-            return cast(
+            task_results = cast(
                 List[Dict[str, Any]],
                 list(self.task_results.find({"root_uid": analysis_id, "status": TaskStatus(status)})),
             )
         else:
-            return cast(List[Dict[str, Any]], list(self.task_results.find({"root_uid": analysis_id})))
+            task_results = cast(List[Dict[str, Any]], list(self.task_results.find({"root_uid": analysis_id})))
+
+        decisions = self._get_decisions_for_task_results(task_results)
+        for task_result in task_results:
+            task_result["decision"] = decisions.get(task_result["uid"], None)
+
+        return task_results
+
+    def get_undecided_task_results_by_analysis_id(self, analysis_id: str) -> List[Dict[str, Any]]:
+        task_results = self.task_result_manual_decisions.find()
+        decisions_for_task_results = self._get_decisions_for_task_results(task_results)
+
+        task_results_filtered = []
+        for task_result in task_results:
+            if task_result["uid"] not in decisions_for_task_results:
+                task_results_filtered.append(task_result)
+        return task_results_filtered
 
     def get_task_by_id(self, task_id: str) -> Optional[Dict[str, Any]]:
-        return cast(Optional[Dict[str, Any]], self.task_results.find_one({"_id": task_id}))
+        task_result = cast(Optional[Dict[str, Any]], self.task_results.find_one({"_id": task_id}))
+        if task_result:
+            task_result["decision"] = self._get_decision_for_task_result(task_result["uid"])
+        return task_result
 
     def save_scheduled_task(self, task: Task) -> bool:
         """
@@ -118,3 +178,44 @@ class DB:
     def task_to_dict(self, task: Task) -> Dict[str, Any]:
         # TODO make this less ugly
         return json.loads(task.serialize())  # type: ignore
+
+    def _get_decision_for_task_result(self, task_result: Dict) -> Optional[TaskResultManualDecision]:
+        decision_dict = self.task_result_manual_decisions.find_one(
+            {"message": task_result["status_reason"]}
+        ) or self.task_result_manual_decisions.find_one(
+            {"message": task_result["status_reason"], "target_str": task_result["target_str"]}
+        )
+
+        if decision_dict:
+            return TaskResultManualDecision(**decision_dict)
+        else:
+            return None
+
+    def _get_decisions_for_task_results(self, task_results: List[Dict]) -> Dict[str, TaskResultManualDecision]:
+        decisions_for_message = {}
+        decisions_for_message_and_target = {}
+
+        for obj in self.task_result_manual_decisions.find():
+            decision = TaskResultManualDecision(**obj)
+
+            if decision.target_string:
+                decisions_for_message_and_target[(decision.message, decision.target_string)] = decision
+            else:
+                decisions_for_message[decision.message] = decision
+
+        decisions = {}
+        for task_result in task_results:
+            if task_result["status"] != TaskStatus.INTERESTING:
+                continue
+
+            decision = None
+
+            if task_result["status_reason"] in decisions_for_message:
+                decision = decisions_for_message[task_result["status_reason"]]
+
+            if (task_result["status_reason"], task_result["target_string"]) in decisions:
+                decision = decisions_for_message[(task_result["status_reason"], task_result["target_string"])]
+
+            if decision:
+                decisions[task_result["uid"]] = decision
+        return decisions

f8b0983e7d8fa3c0562d9c6aff0ae39d17912b4e
Krzysztof Zając <krzysztof.zajac@cert.pl>
2022-12-07 13:04:12 +0100

    Simplified parallelism logic

--- artemis/config.py
+++ artemis/config.py
@@ -12,12 +12,5 @@ class Config:
     POSTMAN_MAIL_TO = getenv("POSTMAN_MAIL_TO", "")
 
     # This determines the http parallelism for asyncio parallel scanning. For each container that runs
-    # the scanning, the number of parallel requests would be:
-    # min(
-    #   CONTAINER_MAX_PARALLEL_HTTP_REQUESTS,
-    #   number of IPs * CONTAINER_PARALLEL_HTTP_REQUESTS_PER_IP
-    # )
-    # This makes sure we use significant parallelism, but only when the number of IPs we scan is
-    # large - if it's not, we don't want to overwhelm any single server.
-    CONTAINER_PARALLEL_HTTP_REQUESTS_PER_IP = int(getenv("CONTAINER_PARALLEL_HTTP_REQUESTS_PER_IP", 5))
+    # the scanning, the number of parallel requests would be CONTAINER_MAX_PARALLEL_HTTP_REQUESTS
     CONTAINER_MAX_PARALLEL_HTTP_REQUESTS = int(getenv("CONTAINER_MAX_PARALLEL_HTTP_REQUESTS", 100))

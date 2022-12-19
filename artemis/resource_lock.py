886bb0f433282f0fb2758d6547d2dec2514b4b36
kazet <krzysztof.zajac@cert.pl>
2022-12-19 12:31:27 +0100

    Default lock expiry (#101)

--- artemis/resource_lock.py
+++ artemis/resource_lock.py
@@ -14,12 +14,13 @@ class ResourceLock:
         self.res_name = res_name
         self.lid = str(uuid4())
 
-    def acquire(self, expiry: Optional[int] = None) -> None:
+    def acquire(self, expiry: Optional[int] = Config.DEFAULT_LOCK_EXPIRY_SECONDS) -> None:
         """
         Acquires a lock.
 
         If expiry is set, the lock will be set to expire after `expiry` seconds
-        (so if expiry is 0, it won't even be acquired).
+        (so if expiry is 0, it won't even be acquired). If it is not set, the lock
+        will expire after Config.DEFAULT_LOCK_EXPIRY_SECONDS seconds.
         """
         if expiry == 0:
             return

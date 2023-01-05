f00b0bec83b0ada412307f78124457bc27b12f01
kazet <krzysztof.zajac@cert.pl>
2023-01-05 19:10:19 +0100

    Larger timeout (#133)

--- artemis/config.py
+++ artemis/config.py
@@ -40,7 +40,7 @@ class Config:
     REDIS = Redis.from_url(decouple.config("REDIS_CONN_STR"))
     ASYNC_REDIS = AsyncRedis.from_url(decouple.config("REDIS_CONN_STR"))
 
-    HTTP_TIMEOUT_SECONDS = decouple.config("HTTP_TIMEOUT_SECONDS", default=2, cast=int)
+    HTTP_TIMEOUT_SECONDS = decouple.config("HTTP_TIMEOUT_SECONDS", default=5, cast=int)
 
     # These two limits are independent - whether the port scanning limits are used doesn't affect
     # the requests limit and vice versa.

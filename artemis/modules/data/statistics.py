f419c409185e832d61db547d815ed994bc7d0524
Krzysztof Zając <krzysztof.zajac@cert.pl>
2023-01-30 13:42:27 +0100

    review fix

--- artemis/modules/data/statistics.py
+++ artemis/modules/data/statistics.py
@@ -1,3 +1,11 @@
-from .bruter_statistics import BRUTER_STATISTICS
+import os
 
-STATISTICS = BRUTER_STATISTICS
+STATISTICS = []
+
+for line in open(os.path.join(os.path.dirname(__file__), "bruter_initial_statistics.txt")):
+    if line:
+        count_str, path = line.strip().split(" ", 1)
+        count_int = int(count_str)
+        STATISTICS.append(
+            {"name": "bruter", "count": count_int, "value": path},
+        )

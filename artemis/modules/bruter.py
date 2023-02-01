aace76f3e353492cfb6568f915233a00101bf269
Krzysztof Zając <krzysztof.zajac@cert.pl>
2023-02-01 09:02:55 +0100

    Removing bruter filtering in case it would be too strict

--- artemis/modules/bruter.py
+++ artemis/modules/bruter.py
@@ -149,23 +149,13 @@ class Bruter(ArtemisBase):
             if response.status_code != 200:
                 continue
 
-            interesting_content = (
-                "<title>phpMyAdmin</title>" in response.content
-                or "<title>phpinfo()</title>" in response.content
-                or "<title>Apache Status</title>" in response.content
-                or "<title>Server Information</title>" in response.content
-                or "<?" in response.content  # php src
-                or "<html" not in response.content[:200].lower()
-                or is_directory_index(response.content)
-            )
-
             filtered_content = (
                 "Error 403" in response.content
                 or response.content.strip() in IGNORED_CONTENTS
                 or SequenceMatcher(None, response.content, dummy_content).quick_ratio() >= 0.8
             )
 
-            if interesting_content and not filtered_content:
+            if not filtered_content:
                 found_urls.append(
                     FoundURL(
                         url=response_url,

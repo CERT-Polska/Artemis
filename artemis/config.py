afe6fa35ca6480fe8b00b08865e710ce6e04fa94
kazet <krzysztof.zajac@cert.pl>
2023-02-07 09:57:31 +0100

    The possibility to add additonal public suffixes (#183)
    * The possibility to add additonal public suffixes
    
    * fix

--- artemis/config.py
+++ artemis/config.py
@@ -13,6 +13,9 @@ class Config:
 
     ALLOW_SCANNING_PUBLIC_SUFFIXES = decouple.config("ALLOW_SCANNING_PUBLIC_SUFFIXES", default=False, cast=bool)
 
+    # additional domains that will be treated as public suffixes (even though they're not on the default Public Suffix List)
+    ADDITIONAL_PUBLIC_SUFFIXES = decouple.config("ADDITIONAL_PUBLIC_SUFFIXES", default="", cast=decouple.Csv(str))
+
     TASK_TIMEOUT_SECONDS = 24 * 3600
 
     # By default, Artemis will check whether the reverse DNS lookup for an IP matches

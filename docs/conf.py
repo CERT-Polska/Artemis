bff35e93989fef22be33543c6c9bb76f4eeaeaaf
Krzysztof Zając <krzysztof.zajac@cert.pl>
2023-02-15 14:57:21 +0100

    Release

--- docs/conf.py
+++ docs/conf.py
@@ -29,7 +29,7 @@ copyright = f"{datetime.datetime.now().year}, CERT Polska"
 author = "CERT Polska"
 
 # The full version, including alpha/beta/rc tags
-release = "0.7.0"
+release = "0.1.0"
 
 latex_engine = "xelatex"
 

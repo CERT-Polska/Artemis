5c30722c33c79cddaeccdd9d7c2abb0befdc7223
kazet <krzysztof.zajac@cert.pl>
2023-02-21 11:40:06 +0100

    Restarting tasks produces a routed task (#216)

--- artemis/karton_utils.py
+++ artemis/karton_utils.py
@@ -1,4 +1,3 @@
-from karton.core import Producer
 from karton.core.backend import KartonBackend
 from karton.core.config import Config as KartonConfig
 from karton.core.inspect import KartonState
@@ -10,10 +9,10 @@ def restart_crashed_tasks() -> None:
     state = KartonState(backend=backend)
 
     for queue in state.queues.values():
-        identity = "artemis.retry"
-        producer = Producer(identity=identity)
-
         for task in queue.crashed_tasks:
             # spawn a new task and mark the original one as finished
-            producer.send_task(task.fork_task())
+            new_task = task.fork_task()
+            new_task.status = TaskState.SPAWNED
+            backend.register_task(new_task)
+            backend.produce_routed_task(task.headers["receiver"], new_task)
             backend.set_task_status(task=task, status=TaskState.FINISHED)

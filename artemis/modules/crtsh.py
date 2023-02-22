80069d75a46004cc978562ada515034b2b75806a
Krzysztof Zając <krzysztof.zajac@cert.pl>
2023-02-22 08:36:26 +0100

    Locking is optional

--- artemis/modules/crtsh.py
+++ artemis/modules/crtsh.py
@@ -23,6 +23,9 @@ class CrtshScanner(ArtemisBase):
     filters = [
         {"type": TaskType.DOMAIN.value},
     ]
+    # We don't ensure we're the only module touching the target, as actually we interact with crt.sh, not
+    # the target
+    lock_target = False
 
     def query_sql(self, domain: str) -> set[str]:
         ct_domains: set[str] = set()

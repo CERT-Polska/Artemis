979b50e7492bc897d243b0392725c7d8dbec002e
Krzysztof Zając <krzysztof.zajac@cert.pl>
2023-02-28 07:47:52 +0100

    Changed Joomla version check so that we ensure newer version for a branch is available

--- test/modules/test_joomla_scanner.py
+++ test/modules/test_joomla_scanner.py
@@ -1,5 +1,6 @@
 from test.base import ArtemisModuleTestCase
 
+import requests_mock
 from karton.core import Task
 
 from artemis.binds import TaskStatus, TaskType, WebApplication
@@ -9,6 +10,30 @@ from artemis.modules.joomla_scanner import JoomlaScanner
 class JoomlaScannerTest(ArtemisModuleTestCase):
     # The reason for ignoring mypy error is https://github.com/CERT-Polska/karton/issues/201
     karton_class = JoomlaScanner  # type: ignore
+
+    def test_is_version_old(self) -> None:
+        with requests_mock.Mocker() as requests_mocker:
+            requests_mocker.get(
+                "https://api.github.com/repos/joomla/joomla-cms/releases",
+                json=[
+                    {
+                        "tag_name": "4.3.0",
+                        "published_at": "2023-02-21T16:00:00Z",
+                    },
+                    {
+                        "tag_name": "3.10.11",
+                        "published_at": "2023-02-21T16:00:00Z",
+                    },
+                ],
+            )
+
+        self.assertTrue(self.karton.is_version_old("2.8.6"))
+        self.assertTrue(self.karton.is_version_old("2.99999.99999"))
+        self.assertTrue(self.karton.is_version_old("3.10.10"))
+        self.assertFalse(self.karton.is_version_old("3.10.11"))
+        self.assertTrue(self.karton.is_version_old("4.0.0"))
+        self.assertFalse(self.karton.is_version_old("4.3.0"))
+        self.assertFalse(self.karton.is_version_old("4.99999.99999"))
 
     def test_simple(self) -> None:
         task = Task(

166a8e1c482c5f6fa0869032325fd5d199639365
Krzysztof Zając <krzysztof.zajac@cert.pl>
2023-03-13 18:43:15 +0100

    Tags are visible on result list and searchable

--- artemis/templating.py
+++ artemis/templating.py
@@ -14,8 +14,9 @@ TEMPLATE_TABLE_ROW_BADGES = templates.get_template("table_row/badges.jinja2")
 def render_table_row(task: Dict[str, Any]) -> List[str]:
     return [
         html.escape(task["created_at"].strftime("%Y-%m-%d %H:%M:%S")) if "created_at" in task else None,
+        html.escape(task["payload_persistent"]["tag"]),
         html.escape(task["headers"]["receiver"]),
         TEMPLATE_TABLE_ROW_TASK_LINK.render({"task": task}),
         TEMPLATE_TABLE_ROW_BADGES.render({"task": task}),
-        task["status_reason"],
+        html.escape(task.get("status_reason") or ""),
     ]

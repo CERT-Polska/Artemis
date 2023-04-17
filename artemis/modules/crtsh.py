af407b1b3c3cdb0cd9d226629ca066bd2a7ff3fa
Krzysztof Zając <krzysztof.zajac@cert.pl>
2023-04-17 12:46:32 +0200

    "_" is allowed in domains

--- artemis/modules/crtsh.py
+++ artemis/modules/crtsh.py
@@ -44,7 +44,7 @@ class CrtshScanner(ArtemisBase):
                 # Escaping LIKE queries is complex:
                 # https://stackoverflow.com/questions/2106207/escape-sql-like-value-for-postgres-with-psycopg2
                 # so for now let's just whitelist domain characters.
-                assert all([c in string.ascii_letters + "-" + string.digits + "." or ord(c) >= 0x80 for c in domain])
+                assert all([c in string.ascii_letters + "-_" + string.digits + "." or ord(c) >= 0x80 for c in domain])
                 cursor.execute(
                     (
                         "SELECT name_value FROM certificate_and_identities"

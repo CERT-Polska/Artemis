3ef575f42ce03802dd07af8545215e1e0c9f53d2
Krzysztof Zając <krzysztof.zajac@cert.pl>
2023-05-15 14:54:47 +0200

    Adding new task copies payload persistent

--- artemis/module_base.py
+++ artemis/module_base.py
@@ -73,6 +73,7 @@ class ArtemisBase(Karton):
 
     def add_task(self, current_task: Task, new_task: Task) -> None:
         new_task.root_uid = current_task.root_uid
+        new_task.payload_persistent.update(current_task.payload_persistent)
         if self.db.save_scheduled_task(new_task):
             self.send_task(new_task)
 

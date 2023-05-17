f41c6d0786875c4b7f889ffd1fc8aab621595dee
Krzysztof Zając <krzysztof.zajac@cert.pl>
2023-05-17 14:05:52 +0200

    Using Karton method to restart tassk

--- artemis/karton_utils.py
+++ artemis/karton_utils.py
@@ -1,7 +1,6 @@
 from karton.core.backend import KartonBackend
 from karton.core.config import Config as KartonConfig
 from karton.core.inspect import KartonState
-from karton.core.task import TaskState
 
 
 def restart_crashed_tasks() -> None:
@@ -10,9 +9,4 @@ def restart_crashed_tasks() -> None:
 
     for queue in state.queues.values():
         for task in queue.crashed_tasks:
-            # spawn a new task and mark the original one as finished
-            new_task = task.fork_task()
-            new_task.status = TaskState.SPAWNED
-            backend.register_task(new_task)
-            backend.produce_routed_task(task.headers["receiver"], new_task)
-            backend.set_task_status(task=task, status=TaskState.FINISHED)
+            backend.restart_task(task)

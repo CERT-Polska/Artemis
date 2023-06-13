f4e1979744eda67d224e4f5678d9cc2cf91453bc
Krzysztof Zając <krzysztof.zajac@cert.pl>
2023-06-13 22:02:55 +0200

    Review fix

--- artemis/domains.py
+++ artemis/domains.py
@@ -11,16 +11,11 @@ def is_main_domain(domain: str) -> bool:
 
 
 def is_subdomain(candidate: str, parent_domain: str, allow_equal: bool = True) -> bool:
-    # Split both domains by dots, so that www.google.com becomes ['www', 'google', 'com']
-    candidate_items = [item for item in candidate.split(".") if item]
-    parent_domain_items = [item for item in parent_domain.split(".") if item]
+    candidate = candidate.strip(".")
+    parent_domain = parent_domain.strip(".")
 
-    # Make sure that candidate_items ends with parent_domain_items
-    # (e.g. ['www', 'google', 'pl'] ends with ['google', 'pl']).
-    #
-    # If allow_equal is set to True, we allow the case that candidate is equal to
-    # parent_domain - if it's not, we expect candidate to be an actual subdomain
-    # of parent_domain.
-    return candidate_items[-len(parent_domain_items) :] == parent_domain_items and (
-        allow_equal or len(candidate_items) != len(parent_domain_items)
-    )
+    if allow_equal and candidate == parent_domain:
+        return True
+
+    tmp = candidate.rstrip(parent_domain)
+    return tmp.endswith(".") and len(tmp) > 1

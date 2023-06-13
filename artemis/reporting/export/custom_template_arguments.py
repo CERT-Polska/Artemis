6fae0e216dc120b9b8758ee5d8ff4a7a16ea09fe
Krzysztof Zając <krzysztof.zajac@cert.pl>
2023-06-13 16:11:01 +0200

    naming fix

--- artemis/reporting/export/custom_template_arguments.py
+++ artemis/reporting/export/custom_template_arguments.py
@@ -1,10 +1,10 @@
 from typing import Dict
 
 
-def parse_custom_template_arguments(custom_template_arguments: str) -> Dict[str, str]:
+def parse_custom_template_arguments(arguments: str) -> Dict[str, str]:
     """Parses custom template arguments in the form of name1=value1,name2=value2,..."""
-    custom_template_arguments_parsed = {}
-    for name_and_value in custom_template_arguments.split(","):
+    arguments_parsed = {}
+    for name_and_value in arguments.split(","):
         name_and_value = name_and_value.strip()
 
         if not name_and_value:
@@ -14,5 +14,5 @@ def parse_custom_template_arguments(custom_template_arguments: str) -> Dict[str,
             raise ValueError("Expected custom_template_arguments to have the form of name1=value1,name2=value2,...")
 
         name, value = name_and_value.split("=", 1)
-        custom_template_arguments_parsed[name] = value
-    return custom_template_arguments_parsed
+        arguments_parsed[name] = value
+    return arguments_parsed

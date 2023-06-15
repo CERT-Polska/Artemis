08c288dae21da66ca78cc674542be24027e2d22b
Krzysztof Zając <krzysztof.zajac@cert.pl>
2023-06-15 14:38:48 +0200

    One missing severity entry

--- artemis/reporting/severity.py
+++ artemis/reporting/severity.py
@@ -46,4 +46,5 @@ SEVERITY_MAP = {
     ReportType("no_https_redirect"): Severity.LOW,
     ReportType("exposed_php_var_dump"): Severity.LOW,
     ReportType("exposed_phpinfo"): Severity.LOW,
+    ReportType("nuclei_exposed_panel"): Severity.LOW,
 }

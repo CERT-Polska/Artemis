7a6a80cb33959f58c3389dbe334e7b045adc326b
Krzysztof Zając <krzysztof.zajac@cert.pl>
2023-06-15 21:48:42 +0200

    dedup fix

--- artemis/reporting/export/deduplication.py
+++ artemis/reporting/export/deduplication.py
@@ -1,11 +1,30 @@
 import collections
 import copy
+import dataclasses
 import datetime
-from typing import Any, DefaultDict, List, Set
+from typing import Any, DefaultDict, List
 
 from artemis.config import Config
+from artemis.reporting.base.normal_form import NormalForm
 from artemis.reporting.base.report import Report
 from artemis.reporting.severity import SEVERITY_MAP, Severity
+
+
+@dataclasses.dataclass
+class ReportsByNormalForms:
+    by_ip_normal_forms: DefaultDict[NormalForm, List[Report]]
+    by_normal_forms: DefaultDict[NormalForm, List[Report]]
+
+    @staticmethod
+    def from_reports(reports: List[Report]) -> "ReportsByNormalForms":
+        by_ip_normal_forms: DefaultDict[NormalForm, List[Report]] = collections.defaultdict(list)
+        by_normal_forms: DefaultDict[NormalForm, List[Report]] = collections.defaultdict(list)
+        for report in reports:
+            by_normal_forms[report.get_normal_form()].append(report)
+            alternative_with_ip_address = report.alternative_with_ip_address()
+            if alternative_with_ip_address:
+                by_ip_normal_forms[alternative_with_ip_address.get_normal_form()].append(alternative_with_ip_address)
+        return ReportsByNormalForms(by_normal_forms=by_normal_forms, by_ip_normal_forms=by_ip_normal_forms)
 
 
 def deduplicate_ip_vs_domain_versions(previous_reports: List[Report], reports_to_send: List[Report]) -> List[Report]:
@@ -18,47 +37,44 @@ def deduplicate_ip_vs_domain_versions(previous_reports: List[Report], reports_to
     - a vulnerability is on a URL with a domain and an identical report for a URL with an IP of this domain has
       already been sent.
     """
-    previous_reports_by_ip_normal_forms: DefaultDict[Any, Any] = collections.defaultdict(list)
-    previous_reports_by_normal_forms: DefaultDict[Any, Any] = collections.defaultdict(list)
-    for previous_report in previous_reports:
-        previous_reports_by_normal_forms[previous_report.get_normal_form()].append(previous_report)
-        alternative_with_ip_address = previous_report.alternative_with_ip_address()
-        if alternative_with_ip_address:
-            previous_reports_by_ip_normal_forms[alternative_with_ip_address.get_normal_form()].append(
-                alternative_with_ip_address
-            )
 
-    reports_to_send_ip_normal_forms: Set[Any] = set()
-    for report in reports_to_send:
-        if not report.target_is_ip_address():
-            alternative_with_ip_address = report.alternative_with_ip_address()
-            if alternative_with_ip_address:
-                reports_to_send_ip_normal_forms.add(alternative_with_ip_address.get_normal_form())
+    previous_reports_normalized = ReportsByNormalForms.from_reports(previous_reports)
+    reports_normalized = ReportsByNormalForms.from_reports(reports_to_send)
+    filtered_reports: List[Report] = []
 
-    reports_to_send_filtered: List[Report] = []
-    for report in reports_to_send:
-        # This is an IP report and is an ip-converted version of an existing report
-        if report.target_is_ip_address() and report.get_normal_form() in previous_reports_by_ip_normal_forms:
-            if _all_reports_are_old(previous_reports_by_ip_normal_forms[report.get_normal_form()]):
-                reports_to_send_filtered.append(_build_subsequent_reminder(report))
-            continue
-        # This is not an IP report but an IP report for the same has already been sent
-        if not report.target_is_ip_address():
-            alternative_with_ip_address = report.alternative_with_ip_address()
-            if (
-                alternative_with_ip_address
-                and alternative_with_ip_address.get_normal_form() in previous_reports_by_normal_forms
+    def _decide_about_ip_report(processed_report: Report) -> None:
+        # This is an ip-converted version of an existing report
+        if processed_report.get_normal_form() in previous_reports_normalized.by_ip_normal_forms:
+            if _all_reports_are_old(previous_reports_normalized.by_ip_normal_forms[processed_report.get_normal_form()]):
+                filtered_reports.append(_build_subsequent_reminder(processed_report))
+            return
+
+        # This is an IP report, but we have a non-ip version to send
+        if processed_report.get_normal_form() in reports_normalized.by_ip_normal_forms:
+            return
+        filtered_reports.append(processed_report)
+
+    def _decide_about_non_ip_report(processed_report: Report) -> None:
+        alternative_with_ip_address = processed_report.alternative_with_ip_address()
+        if (
+            alternative_with_ip_address
+            and alternative_with_ip_address.get_normal_form() in previous_reports_normalized.by_normal_forms
+        ):
+            # This is not an IP report but an IP report for the same has already been sent
+            if _all_reports_are_old(
+                previous_reports_normalized.by_normal_forms[alternative_with_ip_address.get_normal_form()]
             ):
-                if _all_reports_are_old(
-                    previous_reports_by_normal_forms[alternative_with_ip_address.get_normal_form()]
-                ):
-                    reports_to_send_filtered.append(_build_subsequent_reminder(report))
-                continue
-        # This is an IP report but we have a non-ip version to send
-        if report.target_is_ip_address() and report.get_normal_form() in reports_to_send_ip_normal_forms:
-            continue
-        reports_to_send_filtered.append(report)
-    return reports_to_send_filtered
+                filtered_reports.append(_build_subsequent_reminder(processed_report))
+            return
+        filtered_reports.append(processed_report)
+
+    for report in reports_to_send:
+        if report.target_is_ip_address():
+            _decide_about_ip_report(report)
+        else:
+            _decide_about_non_ip_report(report)
+
+    return filtered_reports
 
 
 def deduplicate_reports(previous_reports: List[Report], reports_to_send: List[Report]) -> List[Report]:

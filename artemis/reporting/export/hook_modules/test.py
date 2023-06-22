faaa8d96911d513b573f27fa5f5781f50188fe4a
Krzysztof Zając <krzysztof.zajac@cert.pl>
2023-06-22 16:01:53 +0200

    .

--- artemis/reporting/export/hook_modules/test.py
+++
@@ -1,8 +0,0 @@
-from artemis.reporting.export.export_data import ExportData
-from artemis.reporting.export.hook import ExportHook
-
-
-class TestHook(ExportHook):
-    @staticmethod
-    def run(output_dir: str, export_data: ExportData) -> None:
-        print(output_dir, export_data)

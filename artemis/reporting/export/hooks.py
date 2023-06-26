144eeea22acb68bda6e059094cda6c4a81f3b15d
Krzysztof Zając <krzysztof.zajac@cert.pl>
2023-06-26 16:05:32 +0200

    Review fix

--- artemis/reporting/export/hooks.py
+++ artemis/reporting/export/hooks.py
@@ -10,18 +10,11 @@ from artemis.reporting.export.hook import ExportHook
 def get_all_hooks() -> List[Type[ExportHook]]:
     """Finds all ExportHooks, i.e. all classes inheriting from ExportHook in artemis.reporting.export.hook_modules."""
     modules_dir = os.path.join(os.path.dirname(__file__), "hook_modules")
-    hooks = []
     for item in os.listdir(modules_dir):
         if item.endswith(".py") and item != "__init__.py":
             module_name = item.split(".")[0]
-            import_result = __import__(f"artemis.reporting.export.hook_modules.{module_name}")
-            hook_module = getattr(import_result.reporting.export.hook_modules, module_name)
-            for module_item_name in dir(hook_module):
-                module_item = getattr(hook_module, module_item_name)
-
-                if isinstance(module_item, type) and issubclass(module_item, ExportHook) and module_item != ExportHook:
-                    hooks.append(module_item)
-    return hooks
+            __import__(f"artemis.reporting.export.hook_modules.{module_name}")
+    return ExportHook.__subclasses__
 
 
 def run_export_hooks(output_dir: str, export_data: ExportData) -> None:

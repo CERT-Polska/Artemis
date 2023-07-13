601cafb6dba81cb2b6a5002b8089388d64cd7293
Krzysztof Zając <krzysztof.zajac@cert.pl>
2023-07-13 09:40:57 +0200

    lint

--- docs/generate_config_docs.py
+++ docs/generate_config_docs.py
@@ -1,12 +1,13 @@
 import textwrap
 from typing import get_type_hints
 
-from artemis.config import Config, DEFAULTS
+from artemis.config import DEFAULTS, Config
 
-def print_docs_for_class(cls: type, depth: int=0) -> None:
+
+def print_docs_for_class(cls: type, depth: int = 0) -> None:
     print(cls.__name__)
 
-    header_characters = "-^\""
+    header_characters = '-^"'
     print(header_characters[depth] * len(cls.__name__))
     print()
 
@@ -21,23 +22,23 @@ def print_docs_for_class(cls: type, depth: int=0) -> None:
             continue
         elif member == Config.verify_each_variable_is_annotated:
             continue
-    
-        hint, = hints[variable_name].__metadata__
+
+        (hint,) = hints[variable_name].__metadata__
         indent = 4 * " "
-        doc = "\n".join(
-            textwrap.wrap(hint.strip(), width=100, initial_indent=indent, subsequent_indent=indent)
-        )
+        doc = "\n".join(textwrap.wrap(hint.strip(), width=100, initial_indent=indent, subsequent_indent=indent))
         if variable_name in DEFAULTS:
             default_str = f"{indent}Default: {DEFAULTS[variable_name]}\n\n"
         else:
             default_str = ""
 
         print(
-            textwrap.dedent(f"""
+            textwrap.dedent(
+                f"""
                 {variable_name}\n{default_str}{doc}
             """.strip()
             )
         )
         print()
+
 
 print_docs_for_class(Config)

be74d983ac2efb97db718e969bda73f22fa2c8b0
Krzysztof Zając <krzysztof.zajac@cert.pl>
2023-08-01 10:01:04 +0200

    Metrics

---
+++ artemis/metrics.py
@@ -0,0 +1,57 @@
+import time
+
+from prometheus_client.core import GaugeMetricFamily
+from prometheus_client import REGISTRY, start_http_server, GC_COLLECTOR, PLATFORM_COLLECTOR, PROCESS_COLLECTOR
+
+from karton.core.backend import KartonBackend, KartonMetrics
+from karton.core.config import Config as KartonConfig
+
+from artemis.config import Config
+
+
+class ArtemisMetricsCollector:
+    def __init__(self):
+        pass
+
+    def collect(self):
+        # We check the backend redis queue length directly to avoid the long runtimes of
+        # KartonState.get_all_tasks()
+        backend = KartonBackend(config=KartonConfig())
+
+        yield GaugeMetricFamily(
+            "tasks_consumed",
+            "Karton tasks consumed",
+            value=sum(map(int, backend.redis.hvals(KartonMetrics.TASK_CONSUMED.value))))
+        yield GaugeMetricFamily(
+            "tasks_crashed",
+            "Karton tasks crashed",
+            value=sum(map(int, backend.redis.hvals(KartonMetrics.TASK_CRASHED.value))))
+        yield GaugeMetricFamily(
+            "tasks_queued",
+            "Karton tasks queued",
+            value=sum([backend.redis.llen(key) for key in backend.redis.keys("karton.queue.*")]))
+
+        # We count the number of tasks for these kartons separately as each task pending on them tends to produce
+        # a large number of tasks for other kartons - so we want to monitor the queue length separately.
+        high_level_kartons = ["port_scanner", "crtsh", "gau"]
+
+        num_tasks_high_level_kartons = 0
+        for karton_name in high_level_kartons:
+            num_tasks_high_level_kartons += sum([backend.redis.llen(key) for key in backend.redis.keys(f"karton.queue.*:{karton_name}")])
+
+        yield GaugeMetricFamily(
+            "tasks_queued_high_level_kartons",
+            "Karton tasks queued for high level kartons (e.g. port scanning or subdomain enumeration) that tend to spawn a "
+            "large number of other tasks.",
+            value=num_tasks_high_level_kartons)
+
+
+if __name__ == "__main__":
+    start_http_server(9000)
+    REGISTRY.register(ArtemisMetricsCollector())
+    REGISTRY.unregister(GC_COLLECTOR)
+    REGISTRY.unregister(PLATFORM_COLLECTOR)
+    REGISTRY.unregister(PROCESS_COLLECTOR)
+
+    while True:
+        time.sleep(Config.Miscellaneous.METRICS_EXPORT_INTERVAL)

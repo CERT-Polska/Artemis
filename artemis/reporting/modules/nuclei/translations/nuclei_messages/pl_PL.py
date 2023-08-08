a5d70459b7f102c68ea36545e66085b1e3599d93
kazet <krzysztof.zajac@cert.pl>
2023-08-08 12:56:54 +0200

    Update artemis/reporting/modules/nuclei/translations/nuclei_messages/pl_PL.py
    Co-authored-by: Dominik Tybura <bulexxd@gmail.com>

--- artemis/reporting/modules/nuclei/translations/nuclei_messages/pl_PL.py
+++ artemis/reporting/modules/nuclei/translations/nuclei_messages/pl_PL.py
@@ -363,7 +363,7 @@ TRANSLATIONS = {
     "FTP credentials were detected.": "Wykryto plik ftpsync.settings zawierający dane logowania serwera FTP.",
     "Apache Rocketmq Unauthenticated Access were detected.": "Wykryto, że dostęp  do narzędzia Apache Rocketmq nie wymaga logowania.",
     "Tensorflow Tensorboard was able to be accessed with no authentication requirements in place.": "Wykryto, że dostęp do narzędzia Tensorflow Tensorboard nie wymaga logowania.",
-    "Nginx server is vulnerable to local file inclusion.": "Wykryto, że serwer NGINX jest skonfigurowany w sposób umożliwiający odczyt dowolnych plików z serwera.",
+    "Nginx server is vulnerable to local file inclusion.": "Wykryto, że serwer NGINX jest niepoprawnie skonfigurowany, co umożliwia odczyt dowolnych plików z serwera.",
     "Genie Access WIP3BVAF WISH IP 3MP IR Auto Focus Bullet Camera devices through 3.X are vulnerable to local file inclusion via the web interface, as demonstrated by reading /etc/shadow.": "Wykryto, że urządzenie Genie Access WIP3BVAF WISH IP 3MP IR Auto Focus Bullet Camera zawiera podatność Directory Traversal umożliwiającą odczyt dowolnych plików z dysku."
     + UPDATE_HINT,
     "Bullwark Momentum Series JAWS 1.0 is vulnerable to local file inclusion.": "Wykryto, że Bullwark Momentum Series JAWS 1.0 zawiera podatność Directory Traversal umożliwiającą odczyt dowolnych plików z dysku."

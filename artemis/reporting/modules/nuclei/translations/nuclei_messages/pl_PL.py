8afd961fd4c6f2a68eebb98177ac6d2de353484a
kazet <krzysztof.zajac@cert.pl>
2023-08-08 12:56:39 +0200

    Update artemis/reporting/modules/nuclei/translations/nuclei_messages/pl_PL.py
    Co-authored-by: Dominik Tybura <bulexxd@gmail.com>

--- artemis/reporting/modules/nuclei/translations/nuclei_messages/pl_PL.py
+++ artemis/reporting/modules/nuclei/translations/nuclei_messages/pl_PL.py
@@ -361,7 +361,7 @@ TRANSLATIONS = {
     "The CMNC-200 IP Camera has a built-in web server that is vulnerable to directory transversal attacks, allowing access to any file on the camera file system.": "Wykryto, że kamera CMNC-200 zawiera podatność Directory Traversal umożliwiającą odczyt dowolnych plików z dysku."
     + UPDATE_HINT,
     "FTP credentials were detected.": "Wykryto plik ftpsync.settings zawierający dane logowania serwera FTP.",
-    "Apache Rocketmq Unauthenticated Access were detected.": "Wykryto, że możliwy jest dostęp bez logowania do narzędzia Apache Rocketmq.",
+    "Apache Rocketmq Unauthenticated Access were detected.": "Wykryto, że dostęp  do narzędzia Apache Rocketmq nie wymaga logowania.",
     "Tensorflow Tensorboard was able to be accessed with no authentication requirements in place.": "Wykryto, że dostęp do narzędzia Tensorflow Tensorboard jest możliwy bez logowania.",
     "Nginx server is vulnerable to local file inclusion.": "Wykryto, że serwer NGINX jest skonfigurowany w sposób umożliwiający odczyt dowolnych plików z serwera.",
     "Genie Access WIP3BVAF WISH IP 3MP IR Auto Focus Bullet Camera devices through 3.X are vulnerable to local file inclusion via the web interface, as demonstrated by reading /etc/shadow.": "Wykryto, że urządzenie Genie Access WIP3BVAF WISH IP 3MP IR Auto Focus Bullet Camera zawiera podatność Directory Traversal umożliwiającą odczyt dowolnych plików z dysku."

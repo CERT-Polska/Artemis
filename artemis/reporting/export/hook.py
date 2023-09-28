4d98619cb0566d8bbf47afd6081f68f98fc052db
Krzysztof Zając <krzysztof.zajac@cert.pl>
2023-09-28 16:42:11 +0200

    Hooks have priorities

--- artemis/reporting/export/hook.py
+++ artemis/reporting/export/hook.py
@@ -7,5 +7,10 @@ from artemis.reporting.export.export_data import ExportData
 class ExportHook(ABC):
     @staticmethod
     @abstractmethod
+    def get_priority() -> int:
+        raise NotImplementedError()
+
+    @staticmethod
+    @abstractmethod
     def run(output_dir: Path, export_data: ExportData) -> None:
         raise NotImplementedError()

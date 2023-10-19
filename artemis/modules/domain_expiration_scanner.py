6e942a860fa2334d8cbcabfa8fb8019f36feb303
Anna <anna.madej2@tutanota.com>
2023-10-19 10:32:53 +0200

    Report that the domain will soon expire - rate limit, refactoring (#495)

--- artemis/modules/domain_expiration_scanner.py
+++ artemis/modules/domain_expiration_scanner.py
@@ -4,7 +4,7 @@ import time
 from typing import Any, Dict
 
 from karton.core import Task
-from whois import WhoisQuotaExceeded, query  # type: ignore
+from whois import Domain, WhoisQuotaExceeded, query  # type: ignore
 
 from artemis.binds import TaskStatus, TaskType
 from artemis.config import Config
@@ -27,30 +27,49 @@ class DomainExpirationScanner(ArtemisBase):
         status_reason = None
         if is_main_domain(domain):
             try:
-                now = datetime.datetime.now()
-                domain_data = query(domain)
+                domain_data = self._query_whois(domain=domain)
                 expiry_date = domain_data.expiration_date
-                days_to_expire = None
-                if expiry_date:
-                    days_to_expire = (expiry_date - now).days
-                result["expiration_date"] = expiry_date
-                if (
-                    days_to_expire
-                    and days_to_expire <= Config.Modules.DomainExpirationScanner.DOMAIN_EXPIRATION_TIMEFRAME_DAYS
-                ):
-                    result["close_expiry_date"] = True
-                    result["days_to_expire"] = days_to_expire
-                    status = TaskStatus.INTERESTING
-                    status_reason = (
-                        f"Scanned domain will expire in {days_to_expire} days - (on {expiry_date})."
-                        if days_to_expire != 1
-                        else f"Scanned domain will expire in {days_to_expire} day - (on {expiry_date})."
-                    )
+                result = self._prepare_expiration_data(expiration_date=expiry_date, result=result)
+                status = TaskStatus.INTERESTING
+                status_reason = self._prepare_expiration_status_reason(
+                    days_to_expire=result["days_to_expire"], expiration_date=result["expiration_date"]
+                )
 
             except WhoisQuotaExceeded:
                 time.sleep(24 * 60 * 60)
+                domain_data = self._query_whois(domain=domain)
+                expiry_date = domain_data.expiration_date
+                result = self._prepare_expiration_data(expiration_date=expiry_date, result=result)
+                status = TaskStatus.INTERESTING
+                status_reason = self._prepare_expiration_status_reason(
+                    days_to_expire=result["days_to_expire"], expiration_date=result["expiration_date"]
+                )
 
         self.db.save_task_result(task=current_task, status=status, status_reason=status_reason, data=result)
+
+    @staticmethod
+    def _query_whois(domain: str) -> Domain:
+        return query(domain)
+
+    @staticmethod
+    def _prepare_expiration_data(expiration_date: datetime.datetime, result: Dict[str, Any]) -> Dict[str, Any]:
+        days_to_expire = None
+        now = datetime.datetime.now()
+        if expiration_date:
+            days_to_expire = (expiration_date - now).days
+        result["expiration_date"] = expiration_date
+        if days_to_expire and days_to_expire <= Config.Modules.DomainExpirationScanner.DOMAIN_EXPIRATION_TIMEFRAME_DAYS:
+            result["close_expiry_date"] = True
+            result["days_to_expire"] = days_to_expire
+        return result
+
+    @staticmethod
+    def _prepare_expiration_status_reason(days_to_expire: int, expiration_date: datetime.datetime) -> str:
+        return (
+            f"Scanned domain will expire in {days_to_expire} days - (on {expiration_date})."
+            if days_to_expire != 1
+            else f"Scanned domain will expire in {days_to_expire} day - (on {expiration_date})."
+        )
 
 
 if __name__ == "__main__":

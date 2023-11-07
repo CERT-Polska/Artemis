ee1861bf63be24837fda7d7154469916e3e7fe64
Krzysztof Zając <krzysztof.zajac@cert.pl>
2023-11-07 12:34:52 +0100

    review

--- artemis/reporting/modules/bruter/classifier.py
+++ artemis/reporting/modules/bruter/classifier.py
@@ -86,10 +86,7 @@ def is_sql_dump(found_url: FoundURL) -> bool:
 
     def _starts_with_sql_dump_marker(line: str) -> bool:
         line = line.strip().lower()
-        for marker in sql_dump_markers:
-            if line.startswith(marker):
-                return True
-        return False
+        return any(line.startswith(marker) for marker in sql_dump_markers)
 
     path = urllib.parse.urlparse(found_url.url).path
     if ".sql" not in path.lower() and "/sql" not in path.lower() and "/db" not in path.lower():

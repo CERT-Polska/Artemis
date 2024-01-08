d8d666a094d228e6efaad24107f4a6ec9ddb061b
Matthias Duve <madu@pen-net.de>
2024-01-08 16:40:51 +0100

    Removed not functional test

--- test/modules/test_humble.py
+++ test/modules/test_humble.py
@@ -1,47 +1,41 @@
 import unittest
-from unittest.mock import patch, mock_open, call
-from karton.core import Task
-import subprocess
-from artemis.modules.humble import Humble
-from artemis.binds import TaskStatus
 
-class TestHumble(unittest.TestCase):
-    def setUp(self):
-        self.humble = Humble()
+from artemis.modules import humble
 
-    @patch('subprocess.check_output')
-    @patch('subprocess.run')
-    @patch('builtins.open', new_callable=mock_open, read_data='{"key": "value"}')
-    def test_run_with_messages(self, mock_file, mock_run, mock_output):
-        mock_output.return_value = b'filename'
-        task = Task({"type": "DOMAIN"}, payload={"domain": "test.com"})
-        self.humble.run(task)
-        mock_output.assert_called_with(
-            ["python3", "humble.py", "-u", "https://test.com", "-b", "-o", "json"],
-            cwd="/humble",
-            stderr=subprocess.DEVNULL,
-        )
-        mock_run.assert_called_with(["rm", "filename"])
-        self.humble.db.save_task_result.assert_called_with(
-            task=task, status=TaskStatus.INTERESTING, status_reason='key: value', data={'key': 'value'}
-        )
 
-    @patch('subprocess.check_output')
-    @patch('subprocess.run')
-    @patch('builtins.open', new_callable=mock_open, read_data='')
-    def test_run_without_messages(self, mock_file, mock_run, mock_output):
-        mock_output.return_value = b'filename'
-        task = Task({"type": "DOMAIN"}, payload={"domain": "test.com"})
-        self.humble.run(task)
-        mock_output.assert_called_with(
-            ["python3", "humble.py", "-u", "https://test.com", "-b", "-o", "json"],
-            cwd="/humble",
-            stderr=subprocess.DEVNULL,
-        )
-        mock_run.assert_called_with(["rm", "filename"])
-        self.humble.db.save_task_result.assert_called_with(
-            task=task, status=TaskStatus.OK, status_reason=None, data=[]
-        )
+class TestProcessJsonData(unittest.TestCase):
+    def test_process_json_data_with_valid_input(self) -> None:
+        # Setup
+        input_data = {
+            "[0. Info]": {
+                "Date": "1970/01/01 - 12:12:12",
+                "URL": "https://test.tld"
+            },
+            "[1. Missing HTTP Security Headers]": [
+                "Cache-Control",
+                "Clear-Site-Data", 
+                "Cross-Origin-Embedder-Policy"
+            ],
+            "[2. Fingerprint HTTP Response Headers]": [
+                "Server",
+            ],
+            "[3. Deprecated HTTP Response Headers/Protocols and Insecure Values]": [
+                "X-XSS-Protection (Unsafe Value)"
+            ],
+            "[4. Empty HTTP Response Headers Values]": [
+                "Content-Security-Policy"
+            ],
+            "[5. Browser Compatibility for Enabled HTTP Security Headers]": {
+                "X-XSS-Protection": "https://caniuse.com/?search=X-XSS-Protection"
+            }
+        }
 
-if __name__ == '__main__':
+        # Exercise
+        result = humble.process_json_data(input_data)
+
+        # Verify
+        expected_result = ["Missing http security headers:", "Cache-Control", "Clear-Site-Data", "Cross-Origin-Embedder-Policy"]
+        self.assertEqual(result, expected_result)
+
+if __name__ == "__main__":
     unittest.main()

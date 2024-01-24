030b70c171e10dda8217f7a00a7f25461533455c
Krzysztof Zając <krzysztof.zajac@cert.pl>
2024-01-24 12:32:40 +0100

    WordPress plugin version identifier

---
+++ artemis/modules/wordpress_plugins.py
@@ -0,0 +1,249 @@
+#!/usr/bin/env python3
+import re
+import urllib
+import urllib.parse
+from typing import Any, Dict, List, Optional
+
+import requests
+from bs4 import BeautifulSoup
+from karton.core import Task
+
+from artemis import http_requests
+from artemis.binds import TaskStatus, TaskType, WebApplication
+from artemis.domains import is_subdomain
+from artemis.module_base import ArtemisBase
+
+
+def _get_host_from_url(url: str) -> str:
+    host = urllib.parse.urlparse(url).hostname
+    assert host is not None
+    return host
+
+
+def _get_version_from_readme(slug: str, readme_content: str) -> Optional[str]:
+    previous_line = ""
+    changelog_version = None
+    for line in readme_content.lower().split("\n"):
+        line = line.strip("= #[]")
+        if not line:
+            continue
+
+        if previous_line == "changelog":
+            if line.startswith(slug):
+                line = line[len(slug) :].strip(" :")
+            if line.startswith("version"):
+                line = line[len("version") :].strip(" :")
+            version = (
+                line.replace("(", " ")
+                .replace("[", " ")
+                .replace("]", " ")
+                .replace("'", " ")
+                .replace(":", " ")
+                .replace(",", " ")
+                .strip()
+                .lstrip("v")
+                .split(" ")[0]
+            )
+            if "." in version:
+                changelog_version = version
+                break
+
+        previous_line = line
+
+    tag_lines = [line for line in readme_content.lower().split("\n") if line.strip("* ").startswith("stable tag:")]
+    if len(tag_lines) > 1:
+        return changelog_version
+
+    if len(tag_lines):
+        (tag_line,) = tag_lines
+        tag = tag_line.strip().split(":")[1].strip()
+
+        if tag != "trunk" and (
+            changelog_version is None or tuple(tag.split(".")) > tuple(changelog_version.split("."))
+        ):
+            return tag
+    return changelog_version
+
+
+def _get_plugins_from_homepage(url: str) -> List[Dict[str, Any]]:
+    url_parsed = urllib.parse.urlparse(url)
+    response = http_requests.get(url)
+    soup = BeautifulSoup(response.text)
+    links = []
+    for tag in soup.find_all():
+        new_url = None
+        for attribute in ["src", "href"]:
+            if attribute not in tag.attrs:
+                continue
+
+            new_url = urllib.parse.urljoin(url, tag[attribute])
+            new_url_parsed = urllib.parse.urlparse(new_url)
+
+            if url_parsed.netloc == new_url_parsed.netloc:
+                links.append(new_url)
+
+    plugin_data = []
+    for link in links:
+        pattern = r"\/wp-content\/plugins\/([a-z-0-9]*)\/"
+        match = re.search(pattern, link)
+        if match:
+            plugin_data.append({"slug": match.group(1)})
+
+    return plugin_data
+
+
+class WordpressPluginsScanningException(Exception):
+    pass
+
+
+class WordpressPlugins(ArtemisBase):
+    """
+    Checks whether WordPress plugins are up-to-date.
+    """
+
+    identity = "wordpress_plugins"
+    filters = [
+        {"type": TaskType.WEBAPP.value, "webapp": WebApplication.WORDPRESS.value},
+    ]
+
+    def __init__(self, *args: Any, **kwargs: Any):
+        super().__init__(*args, **kwargs)
+        response = requests.get(
+            "https://api.wordpress.org/plugins/info/1.2/?action=query_plugins&request[page]=1&request[per_page]=1000"
+        )
+        json_response = response.json()
+        self._top_plugins = [
+            {
+                "version": plugin["version"],
+                "slug": plugin["slug"],
+            }
+            for plugin in json_response["plugins"]
+        ]
+
+    def run(self, current_task: Task) -> None:
+        url = current_task.get_payload("url")
+
+        response = http_requests.get(url)
+        if response.is_redirect:
+            redirect_url = response.url
+            self.log.warning("detected a redirect to %s", redirect_url)
+
+            not_scanning_redirect_message = None
+            # The redirect handling is done here, not as a general "module feature", because the handling logic differs
+            # between modules. For example, we may have a site that redirects to other URL, but we still want to check
+            # for /server-status/ or .git, as the redirect may not be complete.
+            #
+            # Here, if the website redirects, the heuristic is as follows:
+            # - if we cannot determine the redirect URL, we naturally cannot redirect,
+            # - if we cannot determine whether the redirect is in scope, because original_domain is not provided, or
+            #   the redirect domain is out of scope (not a subdomain of original_domain), we decide we're not allowed
+            #   to scan,
+            # - if the site redirects to a parent domain (e.g. subdomain.example.com -> example.com) we don't scan as
+            #   it may be the case that old domains are redirected by a wildcard redirect and we would duplicate vulns,
+            # - all other redirects (e.g. domain1.example.com -> domain2.example.com, example.com ->
+            #   subdomain.example.com, ...) are considered interesting and will be scanned.
+            redirect_host = _get_host_from_url(redirect_url)
+            url_host = _get_host_from_url(url)
+            if "original_domain" not in current_task.payload_persistent:
+                not_scanning_redirect_message = (
+                    "The scan was initiated from an IP - not knowing whether the redirect target should be scanned"
+                )
+            elif not redirect_url:
+                not_scanning_redirect_message = "Unable to obtain the redirect url"
+            elif not is_subdomain(redirect_host, current_task.payload_persistent["original_domain"]):
+                not_scanning_redirect_message = (
+                    "The redirect target is not a subdomain of "
+                    f'{current_task.payload_persistent["original_domain"]}, not scanning'
+                )
+            elif is_subdomain(url_host, redirect_host) and url_host != redirect_host:
+                not_scanning_redirect_message = (
+                    "The redirect target is a parent url of a current URL, not scanning as we will return duplicates"
+                )
+            else:
+                self.log.warning("%s redirects to %s, re-running scan", url, redirect_url)
+                url = response.url
+        else:
+            redirect_url = None
+
+        if not_scanning_redirect_message:
+            self.log.warning(
+                not_scanning_redirect_message,
+            )
+            self.db.save_task_result(
+                task=current_task,
+                status=TaskStatus.OK,
+                status_reason=not_scanning_redirect_message,
+            )
+            return
+
+        plugins = {}
+        outdated_plugins = []
+        for plugin in self._top_plugins + _get_plugins_from_homepage(url):
+            response = http_requests.get(url + "/wp-content/plugins/" + plugin["slug"] + "/readme.txt")
+            if "stable tag:" not in response.content.lower():
+                response = http_requests.get(url + "/wp-content/plugins/" + plugin["slug"] + "/README.txt")
+            if "stable tag:" not in response.content.lower():
+                response = http_requests.get(url + "/wp-content/plugins/" + plugin["slug"] + "/readme.md")
+            if "stable tag:" not in response.content.lower():
+                response = http_requests.get(url + "/wp-content/plugins/" + plugin["slug"] + "/README.TXT")
+            if "stable tag:" not in response.content.lower():
+                continue
+
+            version = _get_version_from_readme(plugin["slug"], response.content)
+            if version:
+                plugins[plugin["slug"]] = {
+                    "version": version,
+                }
+
+                if version != plugin["version"]:
+                    outdated_plugins.append(
+                        {
+                            "slug": plugin["slug"],
+                            "version": version,
+                        }
+                    )
+
+        messages = []
+        closed_plugins = []
+        for plugin_slug in plugins.keys():
+            existed = len(requests.get("https://api.wordpress.org/stats/plugin/1.0/" + plugin_slug).json()) > 0
+            plugin_data = requests.get(f"https://api.wordpress.org/plugins/info/1.0/{plugin_slug}.json").json()
+            still_exists = "error" not in plugin_data
+
+            if existed and not still_exists:
+                messages.append(f"Found closed plugin: {plugin_slug}")
+                closed_plugins.append(plugin_slug)
+
+        outdated = []
+        for plugin in outdated_plugins:
+            messages.append(f"Outdated plugin found: {plugin['slug']} {plugin['version']}")
+            outdated.append(
+                {
+                    "type": plugin,
+                    "slug": plugin["slug"],
+                    "version": plugin["version"],
+                }
+            )
+
+        if messages:
+            status = TaskStatus.INTERESTING
+            status_reason = ", ".join(messages)
+        else:
+            status = TaskStatus.OK
+            status_reason = None
+
+        self.db.save_task_result(
+            task=current_task,
+            status=status,
+            status_reason=status_reason,
+            data={
+                "outdated": outdated,
+                "plugins": plugins,
+                "closed_plugins": closed_plugins,
+                "redirect_url": redirect_url,
+            },
+        )
+
+
+if __name__ == "__main__":
+    WordpressPlugins().loop()
---
+++ artemis/reporting/modules/wordpress_plugins/reporter.py
@@ -0,0 +1,146 @@
+import os
+import re
+from functools import lru_cache
+from typing import Any, Callable, Dict, List, Set
+
+import requests
+from packaging import version
+
+from artemis import utils
+from artemis.reporting.base.language import Language
+from artemis.reporting.base.normal_form import NormalForm, get_url_normal_form
+from artemis.reporting.base.report import Report
+from artemis.reporting.base.report_type import ReportType
+from artemis.reporting.base.reporter import Reporter
+from artemis.reporting.base.templating import ReportEmailTemplateFragment
+from artemis.reporting.utils import get_target_url, get_top_level_target
+
+logger = utils.build_logger(__name__)
+
+
+@lru_cache(maxsize=10_000)
+def cached_get_text(url: str) -> str:
+    return requests.get(url).text
+
+
+class WordpressPluginsReporter(Reporter):
+    WORDPRESS_OUTDATED_PLUGIN_THEME = ReportType("wordpress_outdated_plugin_theme")
+    CLOSED_WORDPRESS_PLUGIN = ReportType("closed_wordpress_plugin")
+
+    VERSION_RE = re.compile("Version: <strong>([^<]*)</strong>")
+
+    CLOSED_PLUGINS: Set[str] = set()
+
+    @staticmethod
+    def _is_version_known_to_wordpress(plugin_slug: str, plugin_version: str) -> bool:
+        # Some plugins don't have the latest version as a tag on SVN repo
+        plugin_site_text = cached_get_text(f"https://wordpress.org/plugins/{plugin_slug}/")
+        if re_match := re.search(WordpressPluginsReporter.VERSION_RE, plugin_site_text):
+            (latest_version,) = re_match.groups(1)
+
+            if latest_version == plugin_version:
+                return True
+
+            try:
+                assert isinstance(latest_version, str)
+                if version.parse(latest_version) >= version.parse(plugin_version):
+                    return True
+            except version.InvalidVersion:
+                pass
+
+        return False
+
+    @staticmethod
+    def create_reports(task_result: Dict[str, Any], language: Language) -> List[Report]:
+        if task_result["headers"]["receiver"] != "wordpress_plugins":
+            return []
+
+        if not isinstance(task_result["result"], dict):
+            return []
+
+        result = []
+        for slug in task_result["result"].get("closed_plugins", []):
+            plugin_data = task_result["result"]["plugins"][slug]
+            if plugin_data["version"]:
+                version = plugin_data["version"]
+                version_exists = WordpressPluginsReporter._is_version_known_to_wordpress(slug, version)
+            else:
+                # If no version number, let's assume it's one known to WordPress, so the
+                # plugin is *not* developed separately.
+                version_exists = True
+
+            if version_exists:
+                WordpressPluginsReporter.CLOSED_PLUGINS.add(slug)
+                result.append(
+                    Report(
+                        top_level_target=get_top_level_target(task_result),
+                        target=get_target_url(task_result),
+                        report_type=WordpressPluginsReporter.CLOSED_WORDPRESS_PLUGIN,
+                        additional_data={"slug": slug},
+                        timestamp=task_result["created_at"],
+                    )
+                )
+            else:
+                logger.info(f"{slug} version {version} developed outside WordPress repo")
+
+        for item in task_result["result"]["outdated"]:
+            additional_data = {
+                "type": item["type"],
+                "slug": item["slug"],
+                "version": item["version"],
+            }
+
+            if "redirect_url" in task_result["result"]:
+                additional_data["redirect_url"] = task_result["result"]["redirect_url"]
+
+            result.append(
+                Report(
+                    top_level_target=get_top_level_target(task_result),
+                    target=get_target_url(task_result),
+                    report_type=WordpressPluginsReporter.WORDPRESS_OUTDATED_PLUGIN_THEME,
+                    additional_data=additional_data,
+                    timestamp=task_result["created_at"],
+                )
+            )
+        return result
+
+    @staticmethod
+    def get_email_template_fragments() -> List[ReportEmailTemplateFragment]:
+        return [
+            ReportEmailTemplateFragment.from_file(
+                os.path.join(os.path.dirname(__file__), "template_closed_wordpress_plugin.jinja2"), 6
+            ),
+            ReportEmailTemplateFragment.from_file(
+                os.path.join(os.path.dirname(__file__), "template_wordpress_outdated_plugin_theme.jinja2"), 4
+            ),
+        ]
+
+    @staticmethod
+    def get_scoring_rules() -> Dict[ReportType, Callable[[Report], List[int]]]:
+        """See the docstring in the parent class."""
+        return {
+            WordpressPluginsReporter.CLOSED_WORDPRESS_PLUGIN: Reporter.default_scoring_rule,
+            WordpressPluginsReporter.WORDPRESS_OUTDATED_PLUGIN_THEME: Reporter.default_scoring_rule,
+        }
+
+    @staticmethod
+    def get_normal_form_rules() -> Dict[ReportType, Callable[[Report], NormalForm]]:
+        """See the docstring in the Reporter class."""
+        return {
+            WordpressPluginsReporter.CLOSED_WORDPRESS_PLUGIN: lambda report: Reporter.dict_to_tuple(
+                {
+                    "type": report.report_type,
+                    "target": get_url_normal_form(report.target),
+                    "slug": report.additional_data["slug"],
+                }
+            ),
+            WordpressPluginsReporter.WORDPRESS_OUTDATED_PLUGIN_THEME: lambda report: Reporter.dict_to_tuple(
+                {
+                    "type": report.report_type,
+                    "target": get_url_normal_form(report.target),
+                    "object_type": report.additional_data["type"],
+                    "object_slug": report.additional_data["slug"],
+                    "object_version": report.additional_data["version"],
+                }
+            ),
+        }

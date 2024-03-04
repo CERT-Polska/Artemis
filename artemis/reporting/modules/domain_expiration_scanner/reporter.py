bd28ed353c712dd7646467e4094fb2b7ec91fc98
Krzysztof Zając <krzysztof.zajac2@gmail.com>
2024-03-04 16:51:49 +0100

    Domain expiration scanner fixes related to backend change

--- artemis/reporting/modules/domain_expiration_scanner/reporter.py
+++ artemis/reporting/modules/domain_expiration_scanner/reporter.py
@@ -1,3 +1,4 @@
+import datetime
 import os
 from typing import Any, Dict, List
 
@@ -25,7 +26,7 @@ class DomainExpirationScannerReporter(Reporter):
 
         data = task_result["result"]
         expiration_date_from_result = data["expiration_date"]
-        expiration_date = expiration_date_from_result.strftime("%d-%m-%Y")
+        expiration_date = datetime.datetime.fromisoformat(expiration_date_from_result).strftime("%d-%m-%Y")
 
         return [
             Report(

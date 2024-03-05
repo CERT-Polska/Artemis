a60a5bf1727c2abd9e02b75e9e0358c463c17eea
Krzysztof Zając <krzysztof.zajac2@gmail.com>
2024-03-05 19:18:13 +0100

    .

--- artemis/reporting/modules/domain_expiration_scanner/reporter.py
+++ artemis/reporting/modules/domain_expiration_scanner/reporter.py
@@ -26,7 +26,9 @@ class DomainExpirationScannerReporter(Reporter):
 
         data = task_result["result"]
         expiration_date_from_result = data["expiration_date"]
-        expiration_date = datetime.datetime.fromisoformat(expiration_date_from_result).strftime("%d-%m-%Y")
+        if isinstance(expiration_date_from_result, str):
+            expiration_date_from_result = datetime.datetime.fromisoformat(expiration_date_from_result)
+        expiration_date = expiration_date_from_result.strftime("%d-%m-%Y")
 
         return [
             Report(

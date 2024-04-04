58f84496d44282198e70e8616894995d4426e856
Krzysztof Zając <krzysztof.zajac2@gmail.com>
2024-04-04 11:41:44 +0200

    .

--- artemis/blocklist.py
+++ artemis/blocklist.py
@@ -196,8 +196,8 @@ def blocklist_reports(reports: List[Report], blocklist: List[BlocklistItem]) -> 
                 item.nuclei_template_names
                 and item.report_type in ["nuclei_vulnerability", "nuclei_exposed_panel"]
                 and (
-                    report.additional_data["template_name"] in item.nuclei_template_names or 
-                    report.additional_data["original_template_name"] in item.nuclei_template_names
+                    report.additional_data["template_name"] in item.nuclei_template_names
+                    or report.additional_data["original_template_name"] in item.nuclei_template_names
                 )
             ):
                 continue

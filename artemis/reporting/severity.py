502dd0db9dfdcbf89049af1e9a33a555585671f8
Matie26 <matb20000@gmail.com>
2024-04-15 10:00:03 +0200

    fixed reporting from modules using tasktype device

--- artemis/reporting/severity.py
+++ artemis/reporting/severity.py
@@ -10,6 +10,7 @@ class Severity(str, Enum):
 
 
 SEVERITY_MAP = {
+    ReportType("forti_vuln"): Severity.HIGH,
     ReportType("insecure_wordpress"): Severity.HIGH,
     ReportType("nuclei_vulnerability"): Severity.HIGH,
     ReportType("subdomain_takeover_possible"): Severity.HIGH,

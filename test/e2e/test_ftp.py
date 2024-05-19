a21551ab521299c3a5a6ed0f0ee7adf738ff52ce
Krzysztof Zając <krzysztof.zajac2@gmail.com>
2024-05-19 19:44:03 +0200

    .

--- test/e2e/test_ftp.py
+++ test/e2e/test_ftp.py
@@ -7,16 +7,22 @@ class FTPE2ETestCase(BaseE2ETestCase):
         tag = "ftp-e2e"
         self.submit_tasks([socket.gethostbyname("test-ftp-server-with-easy-password") + ":21"], tag=tag)
         self.wait_for_tasks_finished()
-        self.assertMessagesContain(tag, "Found working credentials for the FTP server: admin:12345, The credentials allow creating files.")
+        self.assertMessagesContain(
+            tag, "Found working credentials for the FTP server: admin:12345, The credentials allow creating files."
+        )
 
     def test_ftp_ip_range(self) -> None:
         tag = "ftp-e2e"
         self.submit_tasks([socket.gethostbyname("test-ftp-server-with-easy-password") + "/32"], tag=tag)
         self.wait_for_tasks_finished()
-        self.assertMessagesContain(tag, "Found working credentials for the FTP server: admin:12345, The credentials allow creating files.")
+        self.assertMessagesContain(
+            tag, "Found working credentials for the FTP server: admin:12345, The credentials allow creating files."
+        )
 
     def test_ftp(self) -> None:
         tag = "ftp-e2e"
         self.submit_tasks([socket.gethostbyname("test-ftp-server-with-easy-password")], tag=tag)
         self.wait_for_tasks_finished()
-        self.assertMessagesContain(tag, "Found working credentials for the FTP server: admin:12345, The credentials allow creating files.")
+        self.assertMessagesContain(
+            tag, "Found working credentials for the FTP server: admin:12345, The credentials allow creating files."
+        )

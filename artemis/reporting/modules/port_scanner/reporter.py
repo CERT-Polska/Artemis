de472b161ee352829ed243262e1b5d8798b1459b
kazet <krzysztof.zajac@cert.pl>
2024-05-20 08:25:45 +0200

    Exposed 3306/5432 (#993)

--- artemis/reporting/modules/port_scanner/reporter.py
+++ artemis/reporting/modules/port_scanner/reporter.py
@@ -12,6 +12,7 @@ from artemis.utils import is_ip_address
 
 class PortScannerReporter(Reporter):
     OPEN_PORT_REMOTE_DESKTOP = ReportType("open_port_remote_desktop")
+    OPEN_PORT_DATABASE = ReportType("open_port_database")
 
     @staticmethod
     def create_reports(task_result: Dict[str, Any], language: Language) -> List[Report]:
@@ -26,18 +27,32 @@ class PortScannerReporter(Reporter):
             assert is_ip_address(ip)
 
             for port, port_data in data.items():
-                if port_data["service"] in ["rdp", "VNC"]:
+                service = port_data["service"].lower()
+                if service in ["mysql", "postgresql", "mssql"]:
+                    result.append(
+                        Report(
+                            top_level_target=get_top_level_target(task_result),
+                            target=f"{service}://{ip}:{port}",
+                            report_type=PortScannerReporter.OPEN_PORT_DATABASE,
+                            additional_data={
+                                "port": port,
+                                "service": service,
+                            },
+                            timestamp=task_result["created_at"],
+                        )
+                    )
+                if service in ["rdp", "vnc"]:
                     if int(port) == 111:
                         continue  # RDPs on this port are false positives
 
                     result.append(
                         Report(
                             top_level_target=get_top_level_target(task_result),
-                            target=f"{port_data['service'].lower()}://{ip}:{port}",
+                            target=f"{service}://{ip}:{port}",
                             report_type=PortScannerReporter.OPEN_PORT_REMOTE_DESKTOP,
                             additional_data={
                                 "port": port,
-                                "service": port_data["service"],
+                                "service": service,
                             },
                             timestamp=task_result["created_at"],
                         )
@@ -49,5 +64,8 @@ class PortScannerReporter(Reporter):
         return [
             ReportEmailTemplateFragment.from_file(
                 os.path.join(os.path.dirname(__file__), "template_open_port_remote_desktop.jinja2"), priority=3
+            ),
+            ReportEmailTemplateFragment.from_file(
+                os.path.join(os.path.dirname(__file__), "template_open_port_database.jinja2"), priority=1
             ),
         ]

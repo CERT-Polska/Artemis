c324b6af9344c84a9a1435b61b088c685c52f832
Krzysztof Zając <krzysztof.zajac2@gmail.com>
2024-05-23 13:01:39 +0200

    .

--- test/e2e/test_ftp.py
+++ test/e2e/test_ftp.py
@@ -4,7 +4,7 @@ from test.e2e.base import BaseE2ETestCase
 
 class FTPE2ETestCase(BaseE2ETestCase):
     def test_ftp_only_port(self) -> None:
-        tag = "ftp-e2e"
+        tag = "ftp-e2e-only-port"
         self.submit_tasks([socket.gethostbyname("test-ftp-server-with-easy-password") + ":21"], tag=tag)
         self.wait_for_tasks_finished()
         self.assertMessagesContain(
@@ -12,7 +12,7 @@ class FTPE2ETestCase(BaseE2ETestCase):
         )
 
     def test_ftp_ip_range(self) -> None:
-        tag = "ftp-e2e"
+        tag = "ftp-e2e-ip-range"
         self.submit_tasks([socket.gethostbyname("test-ftp-server-with-easy-password") + "/32"], tag=tag)
         self.wait_for_tasks_finished()
         self.assertMessagesContain(

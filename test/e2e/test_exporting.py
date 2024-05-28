a2f44b9e2ed08fbdc1ec0cedf23a53d7c6ae3a8b
Krzysztof Zając <krzysztof.zajac2@gmail.com>
2024-05-28 20:36:30 +0200

    .

--- test/e2e/test_exporting.py
+++ test/e2e/test_exporting.py
@@ -65,7 +65,43 @@ class ExportingTestCase(BaseE2ETestCase):
         with zipfile.ZipFile(filename) as export:
             with export.open("messages/test-smtp-server.artemis.html", "r") as f:
                 content = f.read()
-                print(content)
+                print(
+                    content,
+                    "\n".join(
+                        [
+                            "",
+                            "",
+                            "<html>",
+                            "    <head>",
+                            '        <meta charset="UTF-8">',
+                            "    </head>",
+                            "    <style>",
+                            "        ul {",
+                            "            margin-top: 10px;",
+                            "            margin-bottom: 10px;",
+                            "        }",
+                            "    </style>",
+                            "    <body>",
+                            "        <ol>",
+                            "    <li>The following domains don't have properly configured e-mail sender verification mechanisms:        <ul>",
+                            "                    <li>",
+                            "                        test-smtp-server.artemis:",
+                            "",
+                            "                            Valid DMARC record not found. We recommend using all three mechanisms: SPF, DKIM and DMARC to decrease the possibility of successful e-mail message spoofing.",
+                            "                        ",
+                            "                    </li>",
+                            "        </ul>",
+                            "        <p>",
+                            "            These mechanisms greatly increase the chance that the recipient server will reject a spoofed message.",
+                            "            Even if a domain is not used to send e-mails, SPF and DMARC records are needed to reduce the possibility to spoof e-mails.",
+                            "        </p>",
+                            "    </li>",
+                            "        </ol>",
+                            "    </body>",
+                            "</html>",
+                        ]
+                    ),
+                )
                 self.assertEqual(
                     content,
                     "\n".join(
@@ -170,7 +206,43 @@ class ExportingTestCase(BaseE2ETestCase):
         with zipfile.ZipFile(filename) as export:
             with export.open("messages/test-smtp-server.artemis.html", "r") as f:
                 content = f.read()
-                print(content)
+                print(
+                    content,
+                    "\n".join(
+                        [
+                            "",
+                            "",
+                            "<html>",
+                            "    <head>",
+                            '        <meta charset="UTF-8">',
+                            "    </head>",
+                            "    <style>",
+                            "        ul {",
+                            "            margin-top: 10px;",
+                            "            margin-bottom: 10px;",
+                            "        }",
+                            "    </style>",
+                            "    <body>",
+                            "        <ol>",
+                            "    <li>NastÄ\x99pujÄ\x85ce domeny nie majÄ\x85 poprawnie skonfigurowanych mechanizmÃ³w weryfikacji nadawcy wiadomoÅ\x9bci e-mail:        <ul>",
+                            "                    <li>",
+                            "                        test-smtp-server.artemis:",
+                            "",
+                            "                            Nie znaleziono poprawnego rekordu DMARC. Rekomendujemy uÅ¼ywanie wszystkich trzech mechanizmÃ³w: SPF, DKIM i DMARC, aby zmniejszyÄ\x87 szansÄ\x99, Å¼e sfaÅ\x82szowana wiadomoÅ\x9bÄ\x87 zostanie zaakceptowana przez serwer odbiorcy.",
+                            "                        ",
+                            "                    </li>",
+                            "        </ul>",
+                            "        <p>",
+                            "            WdroÅ¼enie tych mechanizmÃ³w znaczÄ\x85co zwiÄ\x99kszy szansÄ\x99, Å¼e serwer odbiorcy odrzuci sfaÅ\x82szowanÄ\x85 wiadomoÅ\x9bÄ\x87 e-mail z powyÅ¼szych domen. W serwisie <a href='https://bezpiecznapoczta.cert.pl'>https://bezpiecznapoczta.cert.pl</a> moÅ¼na zweryfikowaÄ\x87 poprawnoÅ\x9bÄ\x87 implementacji mechanizmÃ³w weryfikacji nadawcy poczty w PaÅ\x84stwa domenie.<br/><br/>WiÄ\x99cej informacji o dziaÅ\x82aniu mechanizmÃ³w weryfikacji nadawcy moÅ¼na znaleÅºÄ\x87 pod adresem <a href='https://cert.pl/posts/2021/10/mechanizmy-weryfikacji-nadawcy-wiadomosci'>https://cert.pl/posts/2021/10/mechanizmy-weryfikacji-nadawcy-wiadomosci</a>.",
+                            "            Nawet w przypadku domeny niesÅ\x82uÅ¼Ä\x85cej do wysyÅ\x82ki poczty rekordy SPF i DMARC sÄ\x85 potrzebne w celu ograniczenia moÅ¼liwoÅ\x9bci podszycia siÄ\x99 pod niÄ\x85. Odpowiednia konfiguracja jest opisana w powyÅ¼szym artykule.",
+                            "        </p>",
+                            "    </li>",
+                            "        </ol>",
+                            "    </body>",
+                            "</html>",
+                        ]
+                    ),
+                )
                 self.assertEqual(
                     content,
                     "\n".join(

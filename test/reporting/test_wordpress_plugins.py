4935c46d2771d7859776d38fec0f30956f0a0d35
Krzysztof Zając <krzysztof.zajac2@gmail.com>
2024-06-13 14:45:02 +0200

    .

--- test/reporting/test_wordpress_plugins.py
+++ test/reporting/test_wordpress_plugins.py
@@ -1,10 +1,14 @@
-from unittest.import TestCase
+from unittest import TestCase
 
-from artemis.reporting.modules.wordpress_plugins.reporter import WordpressPluginsReporter
+from artemis.reporting.modules.wordpress_plugins.reporter import (
+    WordpressPluginsReporter,
+)
 
 
 class WordpressPluginsReporterTest(TestCase):
     def test_is_version_known_to_wordpress(self) -> None:
         self.assertTrue(WordpressPluginsReporter.is_version_known_to_wordpress("sitepress-multilingual-cms", "2.0.4.0"))
         self.assertTrue(WordpressPluginsReporter.is_version_known_to_wordpress("sitepress-multilingual-cms", "2.0.4.1"))
-        self.assertFalse(WordpressPluginsReporter.is_version_known_to_wordpress("sitepress-multilingual-cms", "2.0.4.2"))
+        self.assertFalse(
+            WordpressPluginsReporter.is_version_known_to_wordpress("sitepress-multilingual-cms", "2.0.4.2")
+        )

831e4ed90c726f93f582d1c9b3dcbfa52b0c3092
michalkrzem <michal_krzem@wp.pl>
2024-06-20 09:23:29 +0200

    Alembic files ignored by mypy

--- migrations/env.py
+++ migrations/env.py
@@ -1,9 +1,10 @@
+#  type: ignore
 from logging.config import fileConfig
 
-from sqlalchemy import engine_from_config
-from sqlalchemy import pool
-
 from alembic import context
+from sqlalchemy import engine_from_config, pool
+
+from artemis.db import Base
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -17,7 +18,7 @@ fileConfig(config.config_file_name)
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-from artemis.db import Base
+
 target_metadata = Base.metadata
 
 # target_metadata = None
@@ -66,9 +67,7 @@ def run_migrations_online():
     )
 
     with connectable.connect() as connection:
-        context.configure(
-            connection=connection, target_metadata=target_metadata
-        )
+        context.configure(connection=connection, target_metadata=target_metadata)
 
         with context.begin_transaction():
             context.run_migrations()

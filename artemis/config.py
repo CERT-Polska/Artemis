7111494fe9ad6ca72d62aa7e1612170073040234
Krzysztof Zając <krzysztof.zajac2@gmail.com>
2024-07-11 11:50:30 +0200

    .

--- artemis/config.py
+++ artemis/config.py
@@ -55,7 +55,7 @@ class Config:
     class Reporting:
         REPORTING_MAX_VULN_AGE_DAYS: Annotated[
             int, "When creating e-mail reports, what is the vulnerability maximum age (in days) for it to be reported."
-        ] = get_config("REPORTING_MAX_VULN_AGE_DAYS", default=60, cast=int)
+        ] = get_config("REPORTING_MAX_VULN_AGE_DAYS", default=5, cast=int)
 
         REPORTING_SEPARATE_INSTITUTIONS: Annotated[
             List[str],

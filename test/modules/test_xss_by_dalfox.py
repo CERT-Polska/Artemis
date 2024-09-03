76ea96ec7902cfc65861ecdab9bec8203e8d339a
michalkrzem <michal_krzem@wp.pl>
2024-09-03 23:07:18 +0200

    First tests added

--- test/modules/test_xss_by_dalfox.py
+++ test/modules/test_xss_by_dalfox.py
@@ -1,3 +1,4 @@
+import requests
 from karton.core import Task
 
 from artemis.binds import TaskType, Service
@@ -8,13 +9,34 @@ from test.base import ArtemisModuleTestCase
 class DalFoxTestCase(ArtemisModuleTestCase):
     karton_class = DalFox
 
-    def test_dalfox(self):
+    def test_dalfox_run(self):
         task = Task(
             {"type": TaskType.SERVICE.value, "service": Service.UNKNOWN.value},
-            payload={"url": "http://test-apache-with-sql-injection-xss"},
+            payload={"url": "http://test_apache-with-sql-injection-xss"},
         )
-        # self.run_task(task)
-        # (call,) = self.mock_db.save_task_result.call_args_list
-        #
-        # print(call.kwargs["status"])
+        self.run_task(task)
+        (call,) = self.mock_db.save_task_result.call_args_list
+
+        self.assertEqual(call.kwargs["status"], "INTERESTING")
+
+    def test_dalfox_param_name_with_xss(self):
+        task = Task(
+            {"type": TaskType.SERVICE.value, "service": Service.UNKNOWN.value},
+            payload={"url": "http://test_apache-with-sql-injection-xss/xss.php"},
+        )
+        self.run_task(task)
+        (call,) = self.mock_db.save_task_result.call_args_list
+
+        self.assertTrue(call.kwargs["data"]["result"][0]["param"] == "username")
+        self.assertTrue(False)
+
+    def test_dalfox_data_result_without_xss_error(self):
+        task = Task(
+            {"type": TaskType.SERVICE.value, "service": Service.UNKNOWN.value},
+            payload={"url": "http://test_apache-with-sql-injection-xss/broken.php"},
+        )
+        self.run_task(task)
+        (call,) = self.mock_db.save_task_result.call_args_list
+
+        self.assertTrue(call.kwargs["data"]["result"] == [])
         self.assertTrue(False)

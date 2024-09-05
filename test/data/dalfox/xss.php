cfc3cd3c608ab9d7adc91bc189608c031246ae77
michalkrzem <michal_krzem@wp.pl>
2024-09-05 23:39:47 +0200

    New parameter into xss.php added.

--- test/data/dalfox/xss.php
+++ test/data/dalfox/xss.php
@@ -9,8 +9,14 @@ xss
             <input type="text" id="username" name="username">
             <input type="submit" value="Wyślij">
         </form>
+        <form method="GET">
+            <label for="Password">Podaj hasło:</label>
+            <input type="text" id="password" name="password">
+            <input type="submit" value="Wyślij">
+        </form>
 
         <!-- Wyświetlanie wprowadzonego imienia -->
         <p>Witaj, <?php echo $_GET['username']; ?>!</p>
+        <p>Twoje hasło, <?php echo $_GET['password']; ?>!</p>
     </body>
 </html>

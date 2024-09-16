08047e7ef64ba587c3834bdf78975eb19953765d
michalkrzem <michal_krzem@wp.pl>
2024-09-17 00:35:39 +0200

    If rccode == NOERROR deleted

--- artemis/resolvers.py
+++ artemis/resolvers.py
@@ -44,15 +44,15 @@ def _results_from_answer(domain: str, answer: Answer, result_type: int) -> Set[s
 def _single_resolution_attempt(domain: str, query_type: str = "A") -> Set[str]:
     try:
         answer = dns.resolver.resolve(domain, query_type)
-        if dns.rcode.to_text(answer.response.rcode()) == "NOERROR":
-            if query_type == "A":
-                result_type = 1
-            elif query_type == "NS":
-                result_type = 2
-            else:
-                raise NotImplementedError(f"Don't know how to obtain results for query {query_type}")
 
-            return _results_from_answer(domain, answer, result_type)
+        if query_type == "A":
+            result_type = 1
+        elif query_type == "NS":
+            result_type = 2
+        else:
+            raise NotImplementedError(f"Don't know how to obtain results for query {query_type}")
+
+        return _results_from_answer(domain, answer, result_type)
 
     except dns.resolver.NoAnswer:
         raise NoAnswer()

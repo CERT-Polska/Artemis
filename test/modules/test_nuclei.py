270cb29320975c38bd19ba56ddd8288a537e70d3
Krzysztof Zając <krzysztof.zajac2@gmail.com>
2024-09-20 14:40:51 +0200

    .

--- test/modules/test_nuclei.py
+++ test/modules/test_nuclei.py
@@ -10,25 +10,6 @@ class NucleiTest(ArtemisModuleTestCase):
     # The reason for ignoring mypy error is https://github.com/CERT-Polska/karton/issues/201
     karton_class = Nuclei  # type: ignore
 
-    def test_simple(self) -> None:
-        task = Task(
-            {"type": TaskType.SERVICE.value, "service": Service.HTTP.value},
-            payload={
-                "host": "test-phpmyadmin-easy-password",
-                "port": 80,
-            },
-        )
-        self.run_task(task)
-        (call,) = self.mock_db.save_task_result.call_args_list
-        self.assertEqual(call.kwargs["status"], TaskStatus.INTERESTING)
-        self.assertEqual(
-            call.kwargs["status_reason"],
-            "[high] http://test-phpmyadmin-easy-password:80: phpMyAdmin - Default Login phpMyAdmin contains a "
-            "default login vulnerability. An attacker can obtain access to user accounts and access sensitive "
-            "information, modify data, and/or execute unauthorized operations., [info] http://test-phpmyadmin-"
-            "easy-password:80: phpMyAdmin Panel - Detect phpMyAdmin panel was detected.",
-        )
-
     def test_links(self) -> None:
         task = Task(
             {"type": TaskType.SERVICE.value, "service": Service.HTTP.value},

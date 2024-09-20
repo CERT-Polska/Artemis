0877e572243497a7b4c66fda53d2486b0807d7f8
Krzysztof Zając <krzysztof.zajac2@gmail.com>
2024-09-20 15:41:27 +0200

    .

--- artemis/config.py
+++ artemis/config.py
@@ -521,6 +521,7 @@ class Config:
                         "http/exposures/logs/roundcube-log-disclosure.yaml",
                         "network/detection/rtsp-detect.yaml",
                         "http/miscellaneous/defaced-website-detect.yaml",
+                        "http/misconfiguration/directory-listing-no-host-header.yaml",
                         "http/misconfiguration/django-debug-detect.yaml",
                         "http/misconfiguration/mixed-active-content.yaml",
                         "http/misconfiguration/mysql-history.yaml",

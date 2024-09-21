c7f85ae30666ab8638fe2da5096cd749ab090241
Krzysztof Zając <krzysztof.zajac2@gmail.com>
2024-09-21 18:17:27 +0200

    Revert "."
    This reverts commit 6107179c20bcbd07e5155ea5f05e9b5379f64a30.

--- artemis/modules/nuclei.py
+++ artemis/modules/nuclei.py
@@ -132,6 +132,16 @@ class Nuclei(ArtemisBase):
         if not milliseconds_per_request_initial:
             milliseconds_per_request_initial = 1  # 0 will make Nuclei wait 1 second
 
+        milliseconds_per_request_candidates = [
+            milliseconds_per_request_initial,
+            int(
+                max(
+                    1000 * Config.Modules.Nuclei.NUCLEI_SECONDS_PER_REQUEST_ON_RETRY,
+                    milliseconds_per_request_initial * 2,
+                )
+            ),
+        ]
+
         if Config.Miscellaneous.CUSTOM_USER_AGENT:
             additional_configuration = ["-H", "User-Agent: " + Config.Miscellaneous.CUSTOM_USER_AGENT]
         else:
@@ -139,17 +149,7 @@ class Nuclei(ArtemisBase):
 
         lines = []
         for template_chunk in more_itertools.chunked(templates, Config.Modules.Nuclei.NUCLEI_TEMPLATE_CHUNK_SIZE):
-            milliseconds_per_request_candidates = [
-                milliseconds_per_request_initial,
-                int(
-                    max(
-                        1000 * Config.Modules.Nuclei.NUCLEI_SECONDS_PER_REQUEST_ON_RETRY,
-                        milliseconds_per_request_initial * 2,
-                    )
-                ),
-            ]
-
-            for i, milliseconds_per_request in enumerate(milliseconds_per_request_candidates):
+            for milliseconds_per_request in milliseconds_per_request_candidates:
                 self.log.info(
                     "Running batch of %d templates on %d target(s), milliseconds_per_request=%d",
                     len(template_chunk),
@@ -211,10 +211,15 @@ class Nuclei(ArtemisBase):
                         "Detected %d occurencies of 'context deadline exceeded'",
                         call_result_utf8.count("context deadline exceeded"),
                     )
-                    if i < len(new_milliseconds_per_request_candidates) - 1:
+                    new_milliseconds_per_request_candidates = [
+                        item for item in milliseconds_per_request_candidates if item > milliseconds_per_request
+                    ]
+                    if len(new_milliseconds_per_request_candidates) > 0:
+                        milliseconds_per_request_candidates = new_milliseconds_per_request_candidates
                         self.log.info("Retrying with longer timeout")
                     else:
                         self.log.info("Can't retry with longer timeout")
+
                 else:
                     break
 

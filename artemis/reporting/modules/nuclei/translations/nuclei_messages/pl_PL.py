e3fa564e43e2581e033e3ff2f7a4d6627aae893a
kazet <krzysztof.zajac@cert.pl>
2024-10-01 15:30:31 +0200

    More Nuclei translations (#1295)

--- artemis/reporting/modules/nuclei/translations/nuclei_messages/pl_PL.py
+++ artemis/reporting/modules/nuclei/translations/nuclei_messages/pl_PL.py
@@ -997,7 +997,36 @@ TRANSLATIONS = {
     "Unauthenticated Reflected Cross-Site Scripting (XSS) vulnerability in ThemePunch OHG Essential Grid plugin <= 3.1.0 versions.": "Wtyczka WordPress o nazwie ThemePunch OHG Essential Grid w wersji do 3.1.0 włącznie zawiera podatność "
     + REFLECTED_XSS_DESCRIPTION
     + WORDPRESS_UPDATE_HINT,
+    "HTML Injection in GitHub repository froxlor/froxlor prior to 0.10.38.2.": "Repozytorium froxlor/froxlor w wersji poniźej 0.10.38.2 zawiera podatność "
+    + REFLECTED_XSS_DESCRIPTION
+    + UPDATE_HINT,
+    "WordPress Simple Membership plugin before 4.1.1 contains a reflected cross-site scripting vulnerability. It does not properly sanitize and escape parameters before outputting them back in AJAX actions.": "Wtyczka WordPress o nazwie Simple Membership w wersji poniżej 4.1.1 zawiera podatność "
+    + REFLECTED_XSS_DESCRIPTION
+    + WORDPRESS_UPDATE_HINT,
+    "The Node.js application runs in development mode, which can expose sensitive information, such as source code and secrets, depending on the application.": "Wykryto aplikację w technologii Node.js działającą w trybie deweloperskim. Aplikacje działające w tym trybie mogą potencjalnie udostępniać wrażliwe informacje takie jak kod źródłowy czy konfiguracja aplikacji.",
+    "The remote LDAP server allows anonymous access": "Wykryto serwer LDAP, który nie wymaga logowania. Rekomendujemy, aby takie zasoby nie były publicznie dostępne.",
+    "Jira before version 7.13.4, from version 8.0.0 before version 8.0.4, and from version 8.1.0 before version 8.1.1, allows remote attackers to access files in the Jira webroot under the META-INF directory via local file inclusion.": "System Jira w wersji poniżej 7.13.4, w wersji od 8.0.0 poniżej 8.0.4 i w wersji od 8.1.0 poniżej 8.1.1 umożliwia atakującemu odczytanie plików z dysku."
+    + UPDATE_HINT,
+    "ThinkPHP has a command execution vulnerability because the multi-language function is enabled and the parameter passing of parameter lang is not strictly filtered. Attackers can use this vulnerability to execute commands.": "ThinkPHP zawiera podatnosć umożliwiającą atakującemu zdalne wykonanie kodu."
+    + RCE_EFFECT_DESCRIPTION
+    + WORDPRESS_UPDATE_HINT,
+    "The WP Fastest Cache WordPress plugin before 1.2.2 does not properly sanitise and escape a parameter before using it in a SQL statement, leading to a SQL injection exploitable by unauthenticated users.": "Wtyczka WordPress o nazwie WP Fastest Cache w wersji poniżej 1.2.2 zawiera podatność SQL Injection, umożliwiającą atakującemu pobranie całej zawartości bazy danych."
+    + WORDPRESS_UPDATE_HINT,
+    "The plugin does not validate a parameter passed to the php extract function when loading templates, allowing an unauthenticated attacker to override the template path to read arbitrary files from the hosts file system. This may be escalated to RCE using PHP filter chains.": "Wtyczka WordPress o nazwie Extensive VC Addons for WPBakery w wersji poniżej 1.9.1 zawiera podatność umożliwiającą atakującemu odczyt dowolnych plików z dysku, a w niektórych sytuacjach - zdalne wykonanie kodu. "
+    + RCE_EFFECT_DESCRIPTION
+    + WORDPRESS_UPDATE_HINT,
+    "Detects anonymous access to SMB shares on a remote server.": "Wykryto, że dostęp do udziału SMB jest możliwy bez uwierzytelnienia.",
+    "WordPress Elementor Website Builder plugin before 3.1.4 contains a DOM cross-site scripting vulnerability. It does not sanitize or escape user input appended to the DOM via a malicious hash.": "Wtyczka WordPress o nazwie Elementor Website Builder w wersji poniżej 3.1.4 zawiera podatność "
+    + REFLECTED_XSS_DESCRIPTION
+    + WORDPRESS_UPDATE_HINT,
+    "CHIYU BF-430, BF-431 and BF-450M TCP/IP Converter devices contain a cross-site scripting vulnerability due to a lack of sanitization of the input on the components man.cgi, if.cgi, dhcpc.cgi, and ppp.cgi.": "Urządzenia CHIYU BF-430, BF-431 i BF-450M TCP/IP Converter zawierają podatność "
+    + REFLECTED_XSS_DESCRIPTION
+    + UPDATE_HINT,
+    "RTSP was detected.": "Wykryto publicznie dostępny serwer RTSP.",
+    "Signing is not required on the remote SMB server. An unauthenticated, remote attacker can exploit this to conduct man-in-the-middle attacks against the SMB server.": "Podpisy wiadomości nie są wymagane na serwerze SMB (SigningRequired: false). Dzięki temu atakujący, który może modyfikować komunikację z serwerem SMB, może uzyskać nieuprawniony dostęp do zasobu.",
+    "SSH weak algorithms are outdated cryptographic methods that pose security risks. Identifying and disabling these vulnerable algorithms is crucial for enhancing the overall security of SSH connections.": "Wykryto serwer SSH wspierający słabe/podatne algorytmy kryptograficzne.",
     "javascript/cves/2023/CVE-2023-48795.yaml": "Wykryto implementację protokołu SSH podatną na atak Terrapin, umożliwiający wyłączenie niektórych zabezpieczeń protokołu SSH.",
+    "Subrion CMS before 4.1.5.10 has a SQL injection vulnerability in /front/search.php via the $_GET array.": "System Subrion CMS w wersji poniżej 4.1.5.10 zawiera podatność SQL Injection, umożliwiającą atakującemu pobranie całej zawartości bazy danych.",
     "settings.php source code was detected via backup files.": "Wykryto kopię zapasową pliku settings.php."
     + DATA_HIDE_HINT,
     "WordPress Plugin DB Backup 4.5 and possibly prior versions are prone to a local file inclusion vulnerability because they fail to sufficiently sanitize user-supplied input. Exploiting this issue can allow an attacker to obtain sensitive information that could aid in further attacks.": "Wtyczka WordPress o nazwie DB Backup w wersji 4.5 i potencjalnie wcześniejszych zawiera podatność umożliwiającą atakującemu odczyt dowolnych plików z dysku. "
@@ -1448,4 +1477,5 @@ TRANSLATIONS = {
     "Nagios current status page was detected.": "Wykryto stronę diagnostyczną systemu Nagios.",
     "An Adobe Experience Manager login panel was detected.": "Wykryto panel logowania Adobe Experience Manager.",
     "kiali panel was detected.": "wykryto panel kiali.",
+    "[no description] http/exposed-panels/qBittorrent-panel.yaml": "wykryto panel qBittorrent.",
 }

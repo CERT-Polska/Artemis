e4c4d7335ef1d6e81b0fa9bf1df289fb733ba440
Krzysztof Zając <krzysztof.zajac2@gmail.com>
2024-10-09 18:52:13 +0200

    .

--- artemis/modules/wordpress_plugins.py
+++ artemis/modules/wordpress_plugins.py
@@ -70,7 +70,7 @@ PLUGINS_BAD_VERSION_IN_README = [
     "wc-hide-shipping-methods",
     "website-monetization-by-magenet",
     "woo-tools",
-    'wp-ecommerce-paypal',
+    "wp-ecommerce-paypal",
     "wp-maximum-execution-time-exceeded",
 ]
 

b58319bfee0ccfd7964410b2756e2e315597ebfd
Krzysztof Zając <krzysztof.zajac2@gmail.com>
2024-12-02 14:25:12 +0100

    WordPress plugin version check, Nuclei translations, Nuclei blocklisting

--- artemis/modules/wordpress_plugins.py
+++ artemis/modules/wordpress_plugins.py
@@ -55,6 +55,7 @@ PLUGINS_TO_SKIP_STABLE_TAG = [
 ]
 PLUGINS_BAD_VERSION_IN_README = [
     "blocks-animation",
+    "cf7-styler-for-divi",
     "coming-soon",
     "delete-all-comments-of-website",
     "disable-remove-google-fonts",

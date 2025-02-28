c6e1cb7c149c35b962756ca23da47493f734383b
kazet <krzysztof.zajac@cert.pl>
2025-02-28 11:08:57 +0100

    Making port_scanner faster by batching it (#1556)

--- artemis/modules/port_scanner.py
+++ artemis/modules/port_scanner.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
+import collections
 import json
 import os
 import subprocess
 import time
 from dataclasses import dataclass
-from typing import Any, Dict, Set
+from typing import Any, Dict, List, Set
 
 from karton.core import Task
 
@@ -84,6 +85,8 @@ class PortScanner(ArtemisBase):
         {"type": TaskType.IP.value},
         {"type": TaskType.DOMAIN.value},
     ]
+    batch_tasks = True
+    task_max_batch_size = Config.Modules.PortScanner.PORT_SCANNER_MAX_BATCH_SIZE
 
     @dataclass
     class PortResult:
@@ -91,141 +94,163 @@ class PortScanner(ArtemisBase):
         ssl: bool
         version: str
 
-    def _scan(self, target_ip: str) -> Dict[str, Dict[str, Any]]:
-        # We deduplicate identical tasks, but even if two task are different (e.g. contain
-        # different domain names), they may point to the same IP, and therefore scanning both
-        # would be a waste of resources.
-        if cache := self.cache.get(target_ip):
-            self.log.info(f"host {target_ip} in redis cache")
-            return json.loads(cache)  # type: ignore
+    def _scan(self, target_ips: List[str]) -> Dict[str, Dict[str, Dict[str, Any]]]:
+        result: Dict[str, Dict[str, Dict[str, Any]]] = {}
 
-        self.log.info(f"scanning {target_ip}")
-        time_start = time.time()
-        naabu = subprocess.Popen(
-            [
-                "naabu",
-                "-host",
-                target_ip,
-                "-port",
-                ",".join(map(str, PORTS)),
-                "-silent",
-                "-input-read-timeout",
-                "1s",
-                "-timeout",
-                str(Config.Modules.PortScanner.PORT_SCANNER_TIMEOUT_MILLISECONDS),
-            ]
-            + (
-                ["-rate", str(int(self.requests_per_second_for_current_tasks))]
-                if int(self.requests_per_second_for_current_tasks)
-                else []
-            ),
-            stdout=subprocess.PIPE,
-        )
-        # We don't use `wait()` because of the following warning in the doc:
-        #
-        # This will deadlock when using stdout=PIPE and/or stderr=PIPE and the child process generates enough
-        # output to a pipe such that it blocks waiting for the OS pipe buffer to accept more data. Use
-        # communicate() to avoid that.
-        stdout, stderr = naabu.communicate()
-        if stderr:
-            self.log.info(f"naabu returned the following stderr content: {stderr.decode('utf-8', errors='ignore')}")
+        new_target_ips = []
+        for target_ip in target_ips:
+            # We deduplicate identical tasks, but even if two task are different (e.g. contain
+            # different domain names), they may point to the same IP, and therefore scanning both
+            # would be a waste of resources.
+            if (cache := self.cache.get(target_ip)) is not None:
+                self.log.info(f"host {target_ip} in redis cache")
+                result[target_ip] = json.loads(cache)
+            else:
+                new_target_ips.append(target_ip)
 
-        self.log.info(f"scanning of {target_ip} took {time.time()  - time_start} seconds")
+        if new_target_ips:
+            self.log.info(f"scanning {new_target_ips}")
+            time_start = time.time()
+            naabu = subprocess.Popen(
+                [
+                    "naabu",
+                    "-host",
+                    ",".join(new_target_ips),
+                    "-port",
+                    ",".join(map(str, PORTS)),
+                    "-silent",
+                    "-input-read-timeout",
+                    "1s",
+                    "-timeout",
+                    str(Config.Modules.PortScanner.PORT_SCANNER_TIMEOUT_MILLISECONDS),
+                ]
+                + (
+                    ["-rate", str(int(self.requests_per_second_for_current_tasks))]
+                    if int(self.requests_per_second_for_current_tasks)
+                    else []
+                ),
+                stdout=subprocess.PIPE,
+            )
+            # We don't use `wait()` because of the following warning in the doc:
+            #
+            # This will deadlock when using stdout=PIPE and/or stderr=PIPE and the child process generates enough
+            # output to a pipe such that it blocks waiting for the OS pipe buffer to accept more data. Use
+            # communicate() to avoid that.
+            stdout, stderr = naabu.communicate()
+            if stderr:
+                self.log.info(f"naabu returned the following stderr content: {stderr.decode('utf-8', errors='ignore')}")
 
-        result: Dict[str, Dict[str, Any]] = {}
-        if stdout:
-            lines = stdout.split(b"\n")
-        else:
-            lines = []
+            self.log.info(f"scanning of {new_target_ips} took {time.time()  - time_start} seconds")
 
-        time_start = time.time()
-        lines = [line for line in lines if line]
+            if stdout:
+                lines = stdout.decode("ascii").split("\n")
+            else:
+                lines = []
 
-        for line in lines:
-            ip, port_str = line.split(b":")
+            time_start = time.time()
+            lines = [line for line in lines if line]
+            found_ports: Dict[str, List[str]] = collections.defaultdict(list)
 
-            if len(lines) > Config.Modules.PortScanner.PORT_SCANNER_MAX_NUM_PORTS:
-                self.log.warning(
-                    "We observed more than %s open ports on %s, trimming to most popular ones",
-                    Config.Modules.PortScanner.PORT_SCANNER_MAX_NUM_PORTS,
-                    target_ip,
-                )
-                if int(port_str) not in PORTS_SET_SHORT:
-                    continue
+            for line in lines:
+                ip, port_str = line.split(":")
+                found_ports[ip].append(port_str)
 
-            try:
-                output = self.throttle_request(
-                    lambda: check_output_log_on_error(["fingerprintx", "--json"], self.log, input=line).strip()
-                )
-            except subprocess.CalledProcessError:
-                self.log.exception("Unable to fingerprint %s", line)
-                continue
+            for ip in found_ports.keys():
+                if len(found_ports[ip]) > Config.Modules.PortScanner.PORT_SCANNER_MAX_NUM_PORTS:
+                    self.log.warning(
+                        "We observed more than %s open ports on %s, trimming to most popular ones",
+                        Config.Modules.PortScanner.PORT_SCANNER_MAX_NUM_PORTS,
+                        ip,
+                    )
+                    found_ports[ip] = [port_str for port_str in found_ports[ip] if int(port_str) in PORTS_SET_SHORT]
 
-            if not output:
-                continue
+            for ip in found_ports:
+                for port_str in found_ports[ip]:
+                    try:
+                        output = self.throttle_request(
+                            lambda: check_output_log_on_error(
+                                ["fingerprintx", "--json"], self.log, input=f"{ip}:{port_str}".encode("ascii")
+                            ).strip()
+                        )
+                    except subprocess.CalledProcessError:
+                        self.log.exception("Unable to fingerprint %s", line)
+                        continue
 
-            data = json.loads(output)
-            port = int(data["port"])
-            ssl = data["tls"]
-            service = data["protocol"]
-            version = data.get("version", None) or data.get("metadata", {}).get("fingerprint", None) or "N/A"
-            if ssl:
-                service = service.rstrip("s")
+                    if not output:
+                        continue
 
-            result[str(port)] = self.PortResult(service, ssl, version).__dict__
+                    data = json.loads(output)
+                    port = int(data["port"])
+                    ssl = data["tls"]
+                    service = data["protocol"]
+                    version = data.get("version", None) or data.get("metadata", {}).get("fingerprint", None) or "N/A"
+                    if ssl:
+                        service = service.rstrip("s")
 
-        self.log.info(f"fingerprinting of {target_ip} took {time.time()  - time_start} seconds")
+                    if ip not in result:
+                        result[ip] = {}
+                    result[ip][str(port)] = self.PortResult(service, ssl, version).__dict__
 
-        self.cache.set(target_ip, json.dumps(result).encode("utf-8"))
+            self.log.info(f"fingerprinting of {new_target_ips} took {time.time()  - time_start} seconds")
+
+            for target_ip in new_target_ips:
+                self.cache.set(target_ip, json.dumps(result.get(target_ip, {})).encode("utf-8"))
         return result
 
-    def run(self, current_task: Task) -> None:
-        target = get_target_host(current_task)
-        task_type = current_task.headers["type"]
+    def run_multiple(self, tasks: List[Task]) -> None:
+        hosts_per_task = {}
+        hosts: List[str] = []
 
-        # convert domain to IPs
-        if task_type == TaskType.DOMAIN:
-            hosts = lookup(target)
-        elif task_type == TaskType.IP:
-            hosts = {target}
-        else:
-            raise ValueError("Unknown task type")
+        for task in tasks:
+            target = get_target_host(task)
+            task_type = task.headers["type"]
 
-        all_results = {}
-        open_ports = []
-        interesting_port_descriptions = []
-        for host in hosts:
-            scan_results = self._scan(host)
-            all_results[host] = scan_results
+            # convert domain to IPs
+            if task_type == TaskType.DOMAIN:
+                hosts_per_task[task] = lookup(target)
+            elif task_type == TaskType.IP:
+                hosts_per_task[task] = {target}
+            else:
+                raise ValueError("Unknown task type")
+            hosts.extend(hosts_per_task[task])
 
-            for port, result in all_results[host].items():
-                new_task = Task(
-                    {
-                        "type": TaskType.SERVICE,
-                        "service": Service(result["service"].lower()),
-                    },
-                    payload={
-                        "host": target,
-                        "port": int(port),
-                        "ssl": result["ssl"],
-                    },
-                )
+        scan_results = self._scan(hosts)
 
-                self.add_task(current_task, new_task)
-                open_ports.append(int(port))
+        for task in tasks:
+            all_results = {}
+            open_ports = []
+            interesting_port_descriptions = []
+            for host in hosts_per_task[task]:
+                all_results[host] = scan_results[host]
 
-                interesting_port_descriptions.append(
-                    f"{port} (service: {result['service']} ssl: {result['ssl']}, version: {result['version']})"
-                )
+                for port, result in all_results[host].items():
+                    new_task = Task(
+                        {
+                            "type": TaskType.SERVICE,
+                            "service": Service(result["service"].lower()),
+                        },
+                        payload={
+                            "host": target,
+                            "port": int(port),
+                            "ssl": result["ssl"],
+                        },
+                    )
 
-        if len(interesting_port_descriptions):
-            status = TaskStatus.INTERESTING
-            status_reason = "Found ports: " + ", ".join(sorted(interesting_port_descriptions))
-        else:
-            status = TaskStatus.OK
-            status_reason = None
-        # save raw results
-        self.db.save_task_result(task=current_task, status=status, status_reason=status_reason, data=all_results)
+                    self.add_task(task, new_task)
+                    open_ports.append(int(port))
+
+                    interesting_port_descriptions.append(
+                        f"{port} (service: {result['service']} ssl: {result['ssl']}, version: {result['version']})"
+                    )
+
+            if len(interesting_port_descriptions):
+                status = TaskStatus.INTERESTING
+                status_reason = "Found ports: " + ", ".join(sorted(interesting_port_descriptions))
+            else:
+                status = TaskStatus.OK
+                status_reason = None
+            # save raw results
+            self.db.save_task_result(task=task, status=status, status_reason=status_reason, data=all_results)
 
 
 if __name__ == "__main__":

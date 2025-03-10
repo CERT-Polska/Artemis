b1dd2d0f744699761ca6f7a27134c8f5fe3dd034
kazet <krzysztof.zajac@cert.pl>
2025-03-10 10:44:31 +0100

    Nuclei module stability improvements (#1589)

--- artemis/config.py
+++ artemis/config.py
@@ -158,7 +158,7 @@ class Config:
 
     class Limits:
         TASK_TIMEOUT_SECONDS: Annotated[int, "What is the maximum task run time (after which it will get killed)."] = (
-            get_config("TASK_TIMEOUT_SECONDS", default=6 * 3600, cast=int)
+            get_config("TASK_TIMEOUT_SECONDS", default=12 * 3600, cast=int)
         )
 
         REQUEST_TIMEOUT_SECONDS: Annotated[

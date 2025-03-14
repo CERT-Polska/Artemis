eddc6887325c57ce006f66b40eafe388508dfd8c
kazet <krzysztof.zajac@cert.pl>
2025-03-14 14:52:58 +0100

    Cleanup (#1603)

--- artemis/modules/nuclei.py
+++ artemis/modules/nuclei.py
@@ -55,6 +55,9 @@ class Nuclei(ArtemisBase):
 
         subprocess.call(["git", "clone", "https://github.com/Ostorlab/KEV/", "/known-exploited-vulnerabilities/"])
         with self.lock:
+            # Cleanup so that no old template files exist
+            subprocess.call(["rm", "-rf", "/root/nuclei-templates/*"])
+
             subprocess.call(["nuclei", "-update-templates"])
 
             templates_list_command = ["-tl", "-it", ",".join(TAGS_TO_INCLUDE)]

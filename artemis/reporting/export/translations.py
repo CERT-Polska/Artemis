31cb246f107fac5109c74d2564ad7522761ba6a9
kshitij <82637225+kshitijk4poor@users.noreply.github.com>
2025-03-16 13:28:58 +0530

    add catching missing translations

--- artemis/reporting/export/translations.py
+++ artemis/reporting/export/translations.py
@@ -3,7 +3,7 @@ import os
 import shutil
 import subprocess
 from pathlib import Path
-from typing import Any
+from typing import Any, Dict, Set
 
 from jinja2 import Environment
 
@@ -47,16 +47,88 @@ class TranslationRaiseException(gettext.GNUTranslations):
         raise NotImplementedError()
 
 
+class TranslationCollectMissingException(gettext.GNUTranslations):
+    """This class is used to collect missing translations instead of raising exceptions.
+    It keeps track of all missing translations and returns the original message."""
+
+    # Class variable to collect missing translations across all instances
+    missing_translations: Set[str] = set()
+
+    class _TranslationCollector(gettext.GNUTranslations):
+        def gettext(self, message: str) -> str:
+            # Record missing translation
+            TranslationCollectMissingException.missing_translations.add(message)
+            # Return original message
+            return message
+
+        def ngettext(self, *args: Any) -> Any:
+            raise NotImplementedError()
+
+        def pgettext(self, *args: Any) -> Any:
+            raise NotImplementedError()
+
+        def npgettext(self, *args: Any) -> Any:
+            raise NotImplementedError()
+
+    def __init__(self, fp=None):  # type: ignore
+        super().__init__(fp)
+        self.add_fallback(self._TranslationCollector())
+
+    def gettext(self, message: str) -> str:
+        try:
+            message_translated = super().gettext(message)
+            return message_translated
+        except Exception:
+            # Record missing translation
+            TranslationCollectMissingException.missing_translations.add(message)
+            # Return original message
+            return message
+
+    def ngettext(self, *args: Any) -> Any:
+        raise NotImplementedError()
+
+    def pgettext(self, *args: Any) -> Any:
+        raise NotImplementedError()
+
+    def npgettext(self, *args: Any) -> Any:
+        raise NotImplementedError()
+
+    @classmethod
+    def get_missing_translations(cls) -> Set[str]:
+        """Returns the set of missing translations."""
+        return cls.missing_translations
+
+    @classmethod
+    def clear_missing_translations(cls) -> None:
+        """Clears the set of missing translations."""
+        cls.missing_translations = set()
+
+    @classmethod
+    def save_missing_translations_to_file(cls, file_path: Path) -> None:
+        """Saves the missing translations to a file in .po format."""
+        with open(file_path, 'w') as f:
+            for message in cls.missing_translations:
+                f.write(f'msgid "{message}"\nmsgstr ""\n\n')
+
+
 def install_translations(
     language: Language,
     environment: Environment,
     save_translations_to: Path,
     save_compiled_translations_to: Path,
+    strict_mode: bool = True,
 ) -> None:
     """Collects all .pot files into one, compiles it and installs to Jinja2 environment. Saves the translations
     (both original and compiled) so that they can be used by downstream tools.
 
     We do this as late as possible in order to allow the user to mount additional files as Docker volumes.
+
+    Args:
+        language: The language to install translations for
+        environment: The Jinja2 environment
+        save_translations_to: Path to save the translation file
+        save_compiled_translations_to: Path to save the compiled translation file
+        strict_mode: If True, raises an exception when a translation is missing; if False, returns the original text
     """
     with open(save_translations_to, "w") as all_translations_file:
         for translation_path in Path(__file__).parents[1].glob(f"**/{language.value}/**/*.po"):
@@ -81,10 +153,16 @@ def install_translations(
     )
 
     if language == Language.en_US:  # type: ignore
-        # For English we allow untranslated strings
+        # For English we always allow untranslated strings
         class_ = gettext.GNUTranslations
     else:
-        class_ = TranslationRaiseException
+        # For other languages, use strict or non-strict mode based on the parameter
+        if strict_mode:
+            class_ = TranslationRaiseException
+        else:
+            # Clear any previously collected missing translations
+            TranslationCollectMissingException.clear_missing_translations()
+            class_ = TranslationCollectMissingException
 
     environment.install_gettext_translations(  # type: ignore
         gettext.translation(domain="messages", localedir=".", languages=[language.value], class_=class_)

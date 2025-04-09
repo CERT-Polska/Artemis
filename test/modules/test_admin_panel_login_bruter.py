c313c80b7bec9b5a0c8559d364431a316f46e8ca
Krzysztof Zając <krzysztof.zajac2@gmail.com>
2025-04-09 14:24:05 +0200

    lint

--- test/modules/test_admin_panel_login_bruter.py
+++ test/modules/test_admin_panel_login_bruter.py
@@ -17,13 +17,12 @@ class AdminPanelLoginBruterTest(ArtemisModuleTestCase):
                 "host": "test-php-easy-admin-password",
                 "port": 80,
             },
-        )  
+        )
 
         self.run_task(task)
         (call,) = self.mock_db.save_task_result.call_args_list
         self.assertEqual(call.kwargs["status"], TaskStatus.INTERESTING)
-        self.assertEqual(call.kwargs["data"]["results"][0]['url'], "http://test-php-easy-admin-password/index.php")
-        self.assertEqual(call.kwargs["data"]["results"][0]['username'], "admin")
-        self.assertEqual(call.kwargs["data"]["results"][0]['password'], "admin")
-        self.assertEqual(call.kwargs["data"]["results"][0]['indicators'], ['logout_link', 'no_failure_messages'])
-        
+        self.assertEqual(call.kwargs["data"]["results"][0]["url"], "http://test-php-easy-admin-password/index.php")
+        self.assertEqual(call.kwargs["data"]["results"][0]["username"], "admin")
+        self.assertEqual(call.kwargs["data"]["results"][0]["password"], "admin")
+        self.assertEqual(call.kwargs["data"]["results"][0]["indicators"], ["logout_link", "no_failure_messages"])

da5387c476ed6f9790d7d6e57f70ef9cf1272719
kazet <krzysztof.zajac@cert.pl>
2025-04-24 15:51:34 +0200

    Fixing severity entry (#1698)

--- artemis/reporting/severity.py
+++ artemis/reporting/severity.py
@@ -41,7 +41,7 @@ SEVERITY_MAP = {
     ReportType("misconfigured_email"): Severity.MEDIUM,
     ReportType("old_drupal"): Severity.MEDIUM,
     ReportType("old_joomla"): Severity.MEDIUM,
-    ReportType("xss"): Severity.HIGH,
+    ReportType("xss_vulnerability"): Severity.HIGH,
     ReportType("moodle_vulnerability_found"): Severity.HIGH,
     ReportType("obsolete_moodle_version_found"): Severity.MEDIUM,
     # This doesn't mean that a version is insecure, as WordPress maintains a separate list

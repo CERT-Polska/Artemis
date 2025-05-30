8784affe6178b3a20924cb265cf35e02dd82c74f
Krzysztof Zając <krzysztof.zajac2@gmail.com>
2025-05-30 22:51:00 +0200

    .

--- artemis/module_utils.py
+++ artemis/module_utils.py
@@ -1,4 +1,5 @@
 import os
+
 from artemis.utils import build_logger
 
 
@@ -6,7 +7,7 @@ def try_to_import_all_modules() -> None:
     logger = build_logger(__name__)
     modules_dir = os.path.join(os.path.dirname(__file__), "modules")
     for item in os.listdir(modules_dir):
-        if item.endswith('.py'):
+        if item.endswith(".py"):
             module_name = f"artemis.modules.{item[:-3]}"
             try:
                 __import__(module_name)

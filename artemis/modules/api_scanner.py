63f5cbdd5b15a8c31bf2646264dd57508ef25683
dr4g0n369 <abhinavkarn369@gmail.com>
2025-06-09 09:59:08 +0530

    api scanner using offat and git patch

--- artemis/modules/api_scanner.py
+++ artemis/modules/api_scanner.py
@@ -1,4 +1,5 @@
 import os
+import json
 import tempfile
 from typing import List, Optional, Dict
 
@@ -10,11 +11,7 @@ from artemis.binds import Service, TaskStatus, TaskType
 from artemis.module_base import ArtemisBase
 from artemis.task_utils import get_target_url
 from openapi_spec_validator.readers import read_from_filename
-from openapi_spec_validator import validate
-from offat.generator import TestGenerator
-from offat.tester_utils import is_host_up
-from offat.post_test_processor import PostRunTests
-from offat.utils import parse_server_url, result_to_curl
+from openapi_schema_validator import validate
 from enum import Enum
 
 
@@ -27,254 +24,6 @@ COMMON_SPEC_PATHS = [
     "/api/docs"
     "/docs/swagger.json"
 ]
-
-
-class Findings(Enum):
-    SQLI_VULNERABILITY = "sql_injection_vulnerability"
-
-
-class PayloadFor(Enum):
-    BODY = 0
-    QUERY = 1
-
-
-class APIParser:
-    def __init__(self, file_path: str):
-        self.specification, _ = read_from_filename(file_path)
-        self.base_url = self._get_server_uri()
-
-        self.is_v3 = self._get_oas_version() == 3
-        self.request_response_params = self._get_request_response_params()
-
-        self._populate_hosts()
-
-        if not (self.hosts and self.hosts[0]):
-            raise ValueError('Host is invalid or not found')
-
-        host_dict = self.hosts[0]
-        self.http_scheme = host_dict['scheme']
-        self.host = f'{host_dict["host"]}:{host_dict["port"]}'
-        self.api_base_path = host_dict['basepath']
-        self.base_url = f"{self.http_scheme}://{self.host}"
-
-    def _populate_hosts(self):
-        servers = self.specification.get('servers', [])
-        hosts = []
-        if not servers:
-            self.log.error('Server URLs Not Found in spec file')
-
-        for server in servers:
-            scheme, host, port, basepath = parse_server_url(url=server.get('url'))
-            hosts.append({
-                'scheme': scheme,
-                'host': host,
-                'port': port,
-                'basepath': basepath,
-            })
-
-        self.hosts = hosts
-
-    def _get_oas_version(self):
-        if self.specification.get("openapi"):
-            return 3
-        elif self.specification.get("swagger"):
-            return 2
-        self.log.error("only openapi and swagger specs are supported for now")
-
-    def _get_request_response_params(self):
-        requests = []
-        paths = self.specification.get('paths', {})
-
-        for path in paths.keys():
-            path_params = paths[path].get('parameters', [])
-
-            for http_method in paths.get(path, {}).keys():
-                if http_method not in ['get', 'put', 'post', 'delete', 'options']:
-                    continue
-
-                request_parameters = paths[path][http_method].get('parameters', [])
-                security = paths[path][http_method].get('security', [])
-
-                body_params = []
-                body_parameter_keys = (
-                    paths[path][http_method].get('requestBody', {}).get('content', {})
-                )
-
-                for body_parameter_key in body_parameter_keys:
-                    body_parameters_dict = paths[path][http_method]['requestBody'][
-                        'content'
-                    ][body_parameter_key]
-
-                    required = paths[path][http_method]['requestBody'].get('required')
-                    description = paths[path][http_method]['requestBody'].get(
-                        'description'
-                    )
-                    body_param = self._get_param_definition_schema(body_parameters_dict)
-
-                    body_params.append(
-                        {
-                            'in': 'body',
-                            'name': body_parameter_key,
-                            'description': description,
-                            'required': required,
-                            'schema': body_param,
-                        }
-                    )
-
-                response_params = []
-                response_params = self._get_response_definition_schema(
-                    paths[path][http_method].get('responses', {})
-                )
-
-                request_parameters += body_params
-                requests.append(
-                    {
-                        'http_method': http_method,
-                        'path': path,
-                        'request_params': request_parameters,
-                        'response_params': response_params,
-                        'path_params': path_params,
-                        'body_params': body_params,
-                        'security': security,
-                    }
-                )
-
-        return requests
-
-    def _fetch_schema_from_spec(self, param_schema_ref: str) -> dict:
-        schema_spec_path = param_schema_ref.split('/')[1:]
-
-        if len(schema_spec_path) > 3:
-            return {}
-
-        schema_data: dict = self.specification
-        for child_ele in schema_spec_path:
-            schema_data: dict = schema_data.get(child_ele, {})
-
-        return schema_data
-
-    def _get_param_definition_schema(self, param: Dict):
-        param_schema = param.get('schema')
-        if param_schema:
-            param_schema_ref = param_schema.get('$ref')
-            if param_schema_ref:
-                param_schema = self._fetch_schema_from_spec(param_schema_ref)
-        return param_schema
-
-    def _get_response_definition_schema(self, responses: Dict):
-        for status_code in responses.keys():
-            content = responses[status_code].get('content', None)
-            if content:
-                status_code_content_type_responses = content.keys()
-                for status_code_content_type in status_code_content_type_responses:
-                    status_code_content = responses[status_code]['content'][
-                        status_code_content_type
-                    ].keys()
-                    if 'parameters' in status_code_content:
-                        responses[status_code]['schema'] = responses[status_code][
-                            'content'
-                        ][status_code_content_type]['parameters']
-                    elif 'schema' in status_code_content:
-                        responses[status_code][
-                            'schema'
-                        ] = self._get_param_definition_schema(
-                            responses[status_code]['content'][status_code_content_type]
-                        )
-            else:
-                ref = responses[status_code].get('$ref', None)
-                if ref:
-                    responses[status_code]['schema'] = self._fetch_schema_from_spec(ref)
-        return responses
-
-    def _get_server_uri(self):
-        return self.specification.get("servers")[0].get("url")
-
-
-class TestRunner:
-    def __init__(self):
-        self._client = http_requests
-
-    def _generate_payloads(
-        self, params: List[Dict], payload_for: PayloadFor = PayloadFor.BODY
-    ):
-        if payload_for not in [PayloadFor.BODY, PayloadFor.QUERY]:
-            raise ValueError(
-                '`payload_for` arg only supports `PayloadFor.BODY, PayloadFor.QUERY` value'
-            )
-
-        body_payload = {}
-        query_payload = {}
-
-        for param in params:
-            param_in = param.get('in')
-            param_name = param.get('name')
-            param_value = param.get('value')
-
-            # TODO:handle schema
-
-            match param_in:
-                case 'body':
-                    body_payload[param_name] = param_value
-                case 'query':
-                    query_payload[param_name] = param_value
-                case _:
-                    continue
-
-        match payload_for:
-            case PayloadFor.BODY:
-                return body_payload
-            case PayloadFor.QUERY:
-                return query_payload
-        return {}
-
-    def send_request(self, test_task: Dict):
-        url = test_task.get('url')
-        http_method = test_task.get('method')
-        args = test_task.get('args')
-        kwargs = test_task.get('kwargs', {})
-        body_params = test_task.get('body_params')
-        query_params = test_task.get('query_params')
-
-        if body_params and str(http_method).upper() not in ['GET', 'OPTIONS']:
-            kwargs['json'] = self._generate_payloads(
-                body_params, payload_for=PayloadFor.BODY
-            )
-
-        if query_params:
-            kwargs['params'] = self._generate_payloads(
-                query_params, payload_for=PayloadFor.QUERY
-            )
-
-        test_result = test_task
-        try:
-            response = http_requests.request(
-                url=url, method=http_method, *args, **kwargs
-            )
-            test_result['request_headers'] = response.request.headers
-            test_result['response_headers'] = response.headers
-            test_result['response_body'] = response.text
-            test_result['response_status_code'] = response.status_code
-            test_result['error'] = False
-
-        except Exception as e:
-            test_result['request_headers'] = []
-            test_result['response_headers'] = []
-            test_result['response_body'] = 'No Response Body Found'
-            test_result['response_status_code'] = -1
-            test_result['error'] = e
-
-        test_result['curl_command'] = result_to_curl(test_result)
-        return test_result
-
-    def run_tests(self, test_tasks: List):
-        test_results = []
-        for test_task in test_tasks:
-            test_results.append(self.send_request(test_task))
-
-        test_results = PostRunTests.filter_status_code_based_results(test_results)
-        test_results = PostRunTests.update_result_details(test_results)
-        test_results = PostRunTests.detect_data_exposure(test_results)
-        return test_results
 
 
 class APIResult(BaseModel):
@@ -320,6 +69,24 @@ class APIScanner(ArtemisBase):
                 self.log.debug(f"Error checking {try_url}: {e}")
                 continue
         return None
+    
+    def install_offat(self):
+        if not os.path.exists("/offat"):
+            os.system("git clone https://github.com/OWASP/OFFAT /offat")
+            os.system(f"git -C /offat apply {os.path.join(os.path.dirname(__file__), "data/offat", "offat_artemis.patch")}")
+
+        os.system("pip install -e /offat/src")
+    
+    def scan(self, target_api_specification: str):
+        output_file = "/tmp/output.json"
+        os.system(' '.join(["offat", "-f", target_api_specification, "--only-get-requests", "-o", output_file, "--format", "json"]))
+
+        with open(output_file) as f:
+            file_contents = f.read()
+    
+        report = json.loads(file_contents)
+        os.unlink(output_file)
+        return report
 
     def run(self, current_task: Task) -> None:
         url = get_target_url(current_task)
@@ -334,37 +101,17 @@ class APIScanner(ArtemisBase):
                 data={}
             )
             return
-
+        
         try:
-            api_parser = APIParser(spec_file)
-
-            if not is_host_up(openapi_parser=api_parser):
-                self.db.save_task_result(
-                    task=current_task,
-                    status=TaskStatus.OK,
-                    status_reason="Host is not responding",
-                    data={}
-                )
-                return
-
-            test_generator = TestGenerator()
-            test_runner = TestRunner()
             results = []
-
-            tests_list = []
-            tests_list.append(test_generator.sqli_fuzz_params_test(api_parser))
-            tests_list.append(test_generator.sqli_in_uri_path_fuzz_test(api_parser))
-
-            test_results = []
-            for tests in tests_list:
-                test_results.append(test_runner.run_tests(tests))
+            test_results = self.scan(spec_file)
 
             for result in test_results:
-                if result.get("vulnerable", False) or result.get("vuln_details"):
+                if result.get("vulnerable", False):
                     results.append(APIResult(
                         url=result.get("url"),
                         method=result.get("method"),
-                        vulnerable=True,
+                        vulnerable=result.get("vulnerable"),
                         details=result.get("vuln_details"),
                         curl_command=result.get("curl_command"),
                         status_code=result.get("response_status_code")
@@ -372,7 +119,7 @@ class APIScanner(ArtemisBase):
 
             if results:
                 status = TaskStatus.INTERESTING
-                status_reason = "Found potential SQL injection vulnerabilities"
+                status_reason = "Found potential vulnerabilities in the api"
             else:
                 status = TaskStatus.OK
                 status_reason = None
--- artemis/modules/offat/__init__.py
+++
@@ -1,2 +0,0 @@
-# Copied from MIT-licensed (https://github.com/OWASP/OFFAT/blob/dev/LICENSE.md)
-# Taken from https://github.com/OWASP/OFFAT/tree/dev/src/offat/tester

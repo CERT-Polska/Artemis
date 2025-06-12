4604e1cf3cc8ca2bfa312a3915ce64acc21dccc0
kazet <krzysztof.zajac@cert.pl>
2025-06-12 11:38:26 +0200

    Fewer fps for dmarc check (#1771)

--- artemis/modules/mail_dns_scanner.py
+++ artemis/modules/mail_dns_scanner.py
@@ -47,9 +47,18 @@ class MailDNSScanner(ArtemisBase):
 
         has_mx_records = False
 
-        # Try to find an SMTP for current domain
+        # Try to find an SMTP for current domain or for the private suffix domain - let's treat a domain
+        # as parked if we don't see a MX record on either of them, as some DMARC checks are performed
+        # on the private suffix domain.
         try:
             has_mx_records = len(dns.resolver.resolve(domain, "MX")) > 0
+        except dns.resolver.NoAnswer:
+            pass
+
+        try:
+            has_mx_records = (
+                has_mx_records or len(dns.resolver.resolve(PUBLIC_SUFFIX_LIST.privatesuffix(domain), "MX")) > 0
+            )
         except dns.resolver.NoAnswer:
             pass
 

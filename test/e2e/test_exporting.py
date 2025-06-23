d07e3a7949e267c86c22bc13c22875cd794e00cb
Krzysztof Zając <krzysztof.zajac2@gmail.com>
2025-06-23 12:46:40 +0200

    km

--- test/e2e/test_exporting.py
+++ test/e2e/test_exporting.py
@@ -116,7 +116,6 @@ class ExportingTestCase(BaseE2ETestCase):
 
             with export.open("advanced/output.json", "r") as f:
                 output_data = json.loads(f.read().decode("ascii"))
-                print(output_data)
                 self.assertEqual(list(output_data["messages"].keys()), ["test-smtp-server.artemis"])
                 self.assertEqual(
                     output_data["messages"]["test-smtp-server.artemis"]["reports"][0]["html"],

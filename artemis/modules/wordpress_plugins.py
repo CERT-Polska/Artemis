ea4d44f056593041df4679c28d04759ef52ba8ce
kazet <krzysztof.zajac@cert.pl>
2025-07-07 15:13:25 +0200

    WordPress plugin version identification fix (#1823)

--- artemis/modules/wordpress_plugins.py
+++ artemis/modules/wordpress_plugins.py
@@ -70,7 +70,6 @@ PLUGINS_BAD_VERSION_IN_README = [
     "rafflepress",
     "website-monetization-by-magenet",
     "wp-maximum-execution-time-exceeded",
-    "yellow-pencil-visual-theme-customizer",
 ]
 
 

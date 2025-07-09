6814128d16ed854bd78cb44617478bffa08b79e4
Krzysztof Zając <krzysztof.zajac2@gmail.com>
2025-07-09 09:32:46 +0200

    .

--- test/reporting/test_wp_scanner_autoreporter_integration.py
+++ test/reporting/test_wp_scanner_autoreporter_integration.py
@@ -1,33 +1,22 @@
 from test.base import BaseReportingTest
-from typing import Any, Dict
 
-from karton.core import Task
-
-from artemis.binds import Service, TaskType
-from artemis.modules.wp_scanner import WPScanner
+from artemis.modules.wp_scanner import WordPressScanner
 from artemis.reporting.base.asset import Asset
 from artemis.reporting.base.asset_type import AssetType
-from artemis.reporting.base.language import Language
-from artemis.reporting.base.reporters import (
-    assets_from_task_result,
-    reports_from_task_result,
-)
+from artemis.reporting.base.reporters import assets_from_task_result
 
 
 class WPScannerAutoreporterIntegrationTest(BaseReportingTest):
-    karton_class = WPScanner  # type: ignore
+    karton_class = WordPressScanner  # type: ignore
 
     def test_reporting(self) -> None:
-        data = self.obtain_task_result("wp_scanner", "test-old-wordpress")
+        data = self.obtain_http_task_result("wp_scanner", "test-old-wordpress")
         message = self.task_result_to_message(data)
-        self.assertIn(
-            "The following addresses contain WordPress versions that are no longer", message
-        )
+        self.assertIn("The following addresses contain WordPress versions that are no longer", message)
         self.assertIn("http://test-old-wordpress:80: WordPress 5.9.3", message)
 
     def test_asset_discovery(self) -> None:
-        data = self.obtain_task_result("wp_scanner", "test-old-wordpress")
-        message = self.task_result_to_message(data)
+        data = self.obtain_http_task_result("wp_scanner", "test-old-wordpress")
         assets = assets_from_task_result(data)
         self.assertEqual(
             assets,

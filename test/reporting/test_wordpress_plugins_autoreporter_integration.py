1d21e5bae7b6279331b75ec27d54717d310e3a19
Krzysztof Zając <krzysztof.zajac2@gmail.com>
2025-07-10 14:30:31 +0200

    .

--- test/reporting/test_wordpress_plugins_autoreporter_integration.py
+++ test/reporting/test_wordpress_plugins_autoreporter_integration.py
@@ -5,13 +5,14 @@ from artemis.modules.wordpress_plugins import WordpressPlugins
 from artemis.reporting.base.reporters import assets_from_task_result
 
 
-class WPScannerAutoreporterIntegrationTest(BaseReportingTest):
+class WordpressPluginsScannerAutoreporterIntegrationTest(BaseReportingTest):
     karton_class = WordpressPlugins  # type: ignore
 
     def test_reporting(self) -> None:
         data = self.obtain_webapp_task_result(
             "wordpress_plugins", WebApplication.WORDPRESS, "http://test-old-wordpress:80/"
         )
+        print(data)
         message = self.task_result_to_message(data)
         print(message)
 
@@ -19,5 +20,6 @@ class WPScannerAutoreporterIntegrationTest(BaseReportingTest):
         data = self.obtain_webapp_task_result(
             "wordpress_plugins", WebApplication.WORDPRESS, "http://test-old-wordpress:80/"
         )
+        print(data)
         assets = assets_from_task_result(data)
         print(assets)

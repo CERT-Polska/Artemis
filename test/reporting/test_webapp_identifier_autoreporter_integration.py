2f1582d3126a6531e3328f83ace458fc3899226d
Krzysztof Zając <krzysztof.zajac2@gmail.com>
2025-07-11 08:43:54 +0200

    test fix

--- test/reporting/test_webapp_identifier_autoreporter_integration.py
+++ test/reporting/test_webapp_identifier_autoreporter_integration.py
@@ -1,7 +1,6 @@
 from test.base import BaseReportingTest
 
 from artemis.modules.webapp_identifier import WebappIdentifier
-from artemis.reporting.base.asset import Asset
 from artemis.reporting.base.asset_type import AssetType
 from artemis.reporting.base.reporters import assets_from_task_result
 
@@ -13,45 +12,14 @@ class WebappIdentifierAutoreporterIntegrationTest(BaseReportingTest):
         data = self.obtain_http_task_result("webapp_identifier", "test-old-wordpress")
         assets = assets_from_task_result(data)
         self.assertEqual(
-            set(assets),
+            {(asset.asset_type, asset.name, asset.additional_type, asset.version) for asset in assets},
             {
-                Asset(
-                    asset_type=AssetType.TECHNOLOGY,
-                    name="http://test-old-wordpress:80/",
-                    additional_type="MySQL",
-                ),
-                Asset(
-                    asset_type=AssetType.TECHNOLOGY,
-                    name="http://test-old-wordpress:80/",
-                    additional_type="Debian",
-                ),
-                Asset(
-                    asset_type=AssetType.TECHNOLOGY,
-                    name="http://test-old-wordpress:80/",
-                    additional_type="WordPress Site Editor",
-                ),
-                Asset(
-                    asset_type=AssetType.TECHNOLOGY,
-                    name="http://test-old-wordpress:80/",
-                    additional_type="WordPress Block Editor",
-                ),
-                Asset(
-                    asset_type=AssetType.TECHNOLOGY,
-                    name="http://test-old-wordpress:80/",
-                    additional_type="PHP",
-                    version="7.4.29",
-                ),
-                Asset(
-                    asset_type=AssetType.TECHNOLOGY,
-                    name="http://test-old-wordpress:80/",
-                    additional_type="Apache HTTP Server",
-                    version="2.4.53",
-                ),
-                Asset(
-                    asset_type=AssetType.CMS,
-                    name="http://test-old-wordpress:80/",
-                    additional_type="wordpress",
-                    version="5.9.3",
-                ),
+                (AssetType.TECHNOLOGY, "http://test-old-wordpress:80/", "MySQL", None),
+                (AssetType.TECHNOLOGY, "http://test-old-wordpress:80/", "Debian", None),
+                (AssetType.TECHNOLOGY, "http://test-old-wordpress:80/", "WordPress Site Editor", None),
+                (AssetType.TECHNOLOGY, "http://test-old-wordpress:80/", "WordPress Block Editor", None),
+                (AssetType.TECHNOLOGY, "http://test-old-wordpress:80/", "PHP", "7.4.29"),
+                (AssetType.TECHNOLOGY, "http://test-old-wordpress:80/", "Apache HTTP Server", "2.4.53"),
+                (AssetType.CMS, "http://test-old-wordpress:80/", "wordpress", "5.9.3"),
             },
         )

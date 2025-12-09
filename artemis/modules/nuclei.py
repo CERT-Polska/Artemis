564bd21d3f4359e3fa1760ca23b066b2bc6ecd8d
Krzysztof Zając <krzysztof.zajac@cert.pl>
2025-12-09 15:05:32 +0100

    .

--- artemis/modules/nuclei.py
+++ artemis/modules/nuclei.py
@@ -470,8 +470,8 @@ class Nuclei(ArtemisBase):
         for line in lines:
             if line.strip():
                 finding = json.loads(line)
-                if "template" in finding and finding["template"].startswith(NUCLEI_TEMPLATES_LOCATION):
-                    finding["template"] = finding["template"][len(NUCLEI_TEMPLATES_LOCATION) :]
+                if "template-path" in finding and finding["template-path"].startswith(NUCLEI_TEMPLATES_LOCATION):
+                    finding["template"] = finding["template-path"][len(NUCLEI_TEMPLATES_LOCATION) :]
 
                 findings.append(finding)
         self.log.info(

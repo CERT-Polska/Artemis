40c6f87384bfdea8c26b658e8372a8d67867dce1
Krzysztof Zając <krzysztof.zajac@cert.pl>
2026-02-05 16:49:03 +0100

    WordPress plugin version identification fix

--- artemis/modules/wordpress_plugins.py
+++ artemis/modules/wordpress_plugins.py
@@ -63,10 +63,8 @@ PLUGINS_TO_SKIP_STABLE_TAG = [
     "wow-carousel-for-divi-lite",
 ]
 PLUGINS_BAD_VERSION_IN_README = [
-    "bold-page-builder",
     "coming-soon",
     "famethemes-demo-importer",
-    "gallery-block-lightbox",
     "icon-element",
     "link-manager",
     "login-logo",

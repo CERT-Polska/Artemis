46f9162b2ed7d4f0bc4eb1388b794706f487bbab
Krzysztof Zając <krzysztof.zajac@cert.pl>
2026-03-15 22:10:25 +0100

    lint

--- test/modules/test_subdomain_enumeration.py
+++ test/modules/test_subdomain_enumeration.py
@@ -36,7 +36,7 @@ class SubdomainEnumerationScannerTest(ArtemisModuleTestCase):
     def test_get_subdomains_from_subfinder(self) -> None:
         result = self.karton.get_subdomains_from_subfinder("cert.pl")
         self.assertTrue("ci.drakvuf.cert.pl" in result)
-    
+
     @retry(tries=3, delay=10)
     def test_get_subdomains_from_gau(self) -> None:
         result = self.karton.get_subdomains_from_gau("cert.pl")

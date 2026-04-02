8e6b691c738351768b648896025dd3e948876716
Krzysztof Zając <krzysztof.zajac@cert.pl>
2026-04-02 19:52:41 +0200

    .

--- test/modules/test_subdomain_enumeration.py
+++ test/modules/test_subdomain_enumeration.py
@@ -16,11 +16,11 @@ class TestData(NamedTuple):
 class SubdomainEnumerationScannerTest(ArtemisModuleTestCase):
     # The reason for ignoring mypy error is https://github.com/CERT-Polska/karton/issues/201
     karton_class = SubdomainEnumeration  # type: ignore
-    
+
     @retry(tries=3, delay=10)
     def test_simple(self) -> None:
         data = [TestData("cert.pl", "ci.drakvuf.cert.pl")]
-       
+
         for entry in data:
             task = Task(
                 {"type": TaskType.DOMAIN},
@@ -33,7 +33,7 @@ class SubdomainEnumerationScannerTest(ArtemisModuleTestCase):
                 if item.payload["domain"] == entry.expected_subdomain:
                     found = True
             self.assertTrue(found)
-    
+
     @retry(tries=3, delay=10)
     def test_get_subdomains_from_subfinder(self) -> None:
         result = self.karton.get_subdomains_from_subfinder("cert.pl")

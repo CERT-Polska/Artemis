efe59ea64ec97459d827bff912d1ebaccc0b0fe1
kadewu <krzysztof.waliczek@cert.pl>
2026-04-16 17:51:59 +0200

    try nuclei host-spray strategy

--- artemis/modules/nuclei.py
+++ artemis/modules/nuclei.py
@@ -492,6 +492,8 @@ class Nuclei(ArtemisBase):
                     "1",
                     "-rate-limit-duration",
                     str(milliseconds_per_request) + "ms",
+                    "-ss",
+                    "host-spray",
                     "-stats-json",
                     "-stats-interval",
                     "1",

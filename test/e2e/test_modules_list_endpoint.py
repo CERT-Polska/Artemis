9a94ecb39ab4dee8a5f26ee802e519dc1950e4fc
kazet <krzysztof.zajac@cert.pl>
2026-05-13 10:55:10 +0200

    An endpoint to list modules (#2731)

---
+++ test/e2e/test_modules_list_endpoint.py
@@ -0,0 +1,29 @@
+from test.e2e.base import BACKEND_URL, BaseE2ETestCase
+from typing import Any, Dict, Optional
+
+import requests
+
+API_TOKEN = "api-token"
+VALID_HEADERS: Dict[str, str] = {"X-API-Token": API_TOKEN}
+
+
+class ModulesListEndpointTestCase(BaseE2ETestCase):
+    def test_modules_list_endpoint(self) -> None:
+        response = requests.get(f"{BACKEND_URL}/api/get-modules-that-can-be-disabled", headers=VALID_HEADERS)
+        self.assertEqual(response.status_code, 200)
+
+        data = response.json()
+        self.assertIsInstance(data, list)
+
+        sql_injection_detector_module: Optional[Dict[str, Any]] = None
+
+        for module in data:
+            self.assertIsInstance(module, dict)
+            self.assertIn("identity", module)
+            self.assertIn("info", module)
+            if module["identity"] == "sql_injection_detector":
+                sql_injection_detector_module = module
+
+        self.assertTrue(sql_injection_detector_module)
+        self.assertEqual(sql_injection_detector_module["identity"], "sql_injection_detector")  # type: ignore
+        self.assertTrue("🔴 Scanned system load/risk: high." in sql_injection_detector_module["info"])  # type: ignore

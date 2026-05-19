c92d6e331b17c98908d50fe6e5c8ba893b7808b4
Krzysztof Zając <krzysztof.zajac@cert.pl>
2026-05-19 17:53:35 +0200

    Revert "breaking test"
    This reverts commit d8a557148c1ab4b510083f1f3fc9ec0353b22bb6.

--- test/modules/test_bruter.py
+++ test/modules/test_bruter.py
@@ -43,7 +43,7 @@ class BruterTest(ArtemisModuleTestCase):
                 call.kwargs["data"]["found_urls"],
                 [
                     {
-                        "url": "http://test-service-with-bruteable-files:80/config.dit",
+                        "url": "http://test-service-with-bruteable-files:80/config.dist",
                         "content_prefix": "...\n",
                         "has_directory_index": False,
                     },

87ad572de65157d6bfa92ce9b39d712b79ff8530
Krzysztof Zając <krzysztof.zajac@cert.pl>
2026-06-11 14:23:58 +0200

    .

--- test/modules/test_ip_lookup.py
+++ test/modules/test_ip_lookup.py
@@ -10,18 +10,18 @@ from artemis.modules.ip_lookup import IPLookup
 class IPLookupTest(ArtemisModuleTestCase):
     karton_class = IPLookup  # type: ignore
 
-    @patch("artemis.modules.ip_lookup.lookup", return_value={"146.59.80.63"})
+    @patch("artemis.modules.ip_lookup.lookup", return_value={"1.2.3.4"})
     def test_simple(self, mock_lookup) -> None:  # type: ignore
         task = Task(
             {"type": TaskType.DOMAIN},
-            payload={TaskType.DOMAIN.value: "lebihan.pl"},
+            payload={TaskType.DOMAIN.value: "test.example"},
         )
         results = self.run_task(task)
 
         expected_task = Task(
             {"type": TaskType.NEW, "origin": IPLookup.identity},
-            payload={"data": "146.59.80.63", "last_domain": "lebihan.pl"},
+            payload={"data": "1.2.3.4", "last_domain": "test.example"},
         )
         del results[0].payload["created_at"]
         self.assertTasksEqual(results, [expected_task])
-        mock_lookup.assert_called_once_with("lebihan.pl")
+        mock_lookup.assert_called_once_with("test.example")

6cfaf24c83948dcd44fc1de77b84aa735537d356
kadewu <krzysztof.waliczek@cert.pl>
2026-06-19 16:02:43 +0200

    Remove logic for dangling IP - as it currently generates too much noise and FP

--- test/external_network/test_dangling_dns_detector.py
+++ test/external_network/test_dangling_dns_detector.py
@@ -1,5 +1,4 @@
 from test.base import ArtemisModuleTestCase
-from unittest.mock import patch
 
 from karton.core import Task
 
@@ -29,26 +28,4 @@ class TestDanglingDnsDetectorIntegration(ArtemisModuleTestCase):
         self.assertTrue(
             "The defined domain has a CNAME record configured but the CNAME does not resolve."
             in call.kwargs["status_reason"],
-        )
-
-    def test_check_dns_ip_records_integration(self) -> None:
-        # given
-        task = Task(
-            {"type": TaskType.DOMAIN_THAT_MAY_NOT_EXIST.value},
-            payload={
-                "domain": "dangling.test.artemis.lab.cert.pl",
-                "last_domain": "dangling.test.artemis.lab.cert.pl",
-            },
-        )
-
-        # when
-        with patch("artemis.config.Config.Modules.DanglingDnsDetector") as mocked_config:
-            mocked_config.DANGLING_DNS_NUMBER_OF_RETRIES_FOR_IP = 0
-            self.run_task(task)
-            (call,) = self.mock_db.save_task_result.call_args_list
-
-        # then
-        self.assertEqual(call.kwargs["status"], TaskStatus.INTERESTING)
-        self.assertTrue(
-            "The defined domain has an A record configured but the IP does not resolve." in call.kwargs["status_reason"]
         )

8432af3007c115f8de86ae00c863f21d094a112a
Krzysztof Zając <krzysztof.zajac@cert.pl>
2026-07-06 17:42:16 +0200

    Translations

--- artemis/reporting/modules/mail_dns_scanner/reporter.py
+++ artemis/reporting/modules/mail_dns_scanner/reporter.py
@@ -19,6 +19,7 @@ from artemis.reporting.base.templating import ReportEmailTemplateFragment
 from artemis.reporting.utils import get_top_level_target
 
 SSL_ERRORS_TO_SKIP = [
+    "Certificate error: unable to get local issuer certificate",
     "2.0-OpenSSH",
     "4.3.2 try again later",
     "(554, b'5.7.1 Delivery not authorized')",

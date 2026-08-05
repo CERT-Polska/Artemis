68afd8506b444f544bbd24f8ba31882eed326659
kadewu <krzysztof.waliczek@cert.pl>
2026-08-05 14:21:55 +0200

    move flaky external test to corresponding group (#3010)

--- test/unit/test_web_technology_identification.py
+++ test/unit/test_web_technology_identification.py
@@ -51,26 +51,6 @@ class TestWebTechnologyIdentification(unittest.TestCase):
             (apache,) = [tech for tech in tech_results[target] if tech.name == "Apache HTTP Server"]
             self.assertEqual(apache.cpe, "cpe:2.3:a:apache:http_server:*:*:*:*:*:*:*:*")
 
-    def test_skipping_ssl_verification(self) -> None:
-        targets = ["https://self-signed.badssl.com"]
-
-        logger = logging.Logger("test_logger")
-        tech_results = run_tech_detection(targets, logger=logger)
-
-        # Without skipping SSL verification, the output is empty and the below error is logged:
-        # Error fetching https://self-signed.badssl.com/: Get "https://self-signed.badssl.com/": tls: failed to verify certificate: x509: certificate signed by unknown authority
-        expected_results = {
-            "https://self-signed.badssl.com": [
-                "Ubuntu",
-                "Nginx:1.10.3",
-            ],
-        }
-
-        for target in targets:
-            self.assertIn(target, tech_results)
-            detected_tags = set(to_tag_strings(tech_results[target]))
-            self.assertEqual(detected_tags, set(expected_results[target]))
-
     @patch("artemis.web_technology_identification.subprocess.check_output")
     @patch("artemis.web_technology_identification.subprocess.run")
     @patch("artemis.web_technology_identification.os.path.exists", return_value=True)

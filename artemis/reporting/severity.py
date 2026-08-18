5d4be3f0d816b2a50a34b93787ada3bebc23d744
Krzysztof Zając <krzysztof.zajac@cert.pl>
2026-08-18 16:02:09 +0200

    .

--- artemis/reporting/severity.py
+++ artemis/reporting/severity.py
@@ -15,6 +15,7 @@ class Severity(str, Enum):
 SEVERITY_MAP = {
     ReportType("xss_scanner"): Severity.HIGH,
     ReportType("forti_vuln"): Severity.HIGH,
+    ReportType("command_injection"): Severity.HIGH,
     ReportType("globalprotect_vuln"): Severity.HIGH,
     ReportType("lfi_vulnerability"): Severity.HIGH,
     ReportType("rce_vulnerability"): Severity.HIGH,

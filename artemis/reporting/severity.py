6d1936b744600935f54aab5b87a9b8307ca8fc3f
Krzysztof Zając <krzysztof.zajac@cert.pl>
2026-08-18 16:29:03 +0200

    Revert "."
    This reverts commit 5d4be3f0d816b2a50a34b93787ada3bebc23d744.

--- artemis/reporting/severity.py
+++ artemis/reporting/severity.py
@@ -15,7 +15,6 @@ class Severity(str, Enum):
 SEVERITY_MAP = {
     ReportType("xss_scanner"): Severity.HIGH,
     ReportType("forti_vuln"): Severity.HIGH,
-    ReportType("command_injection"): Severity.HIGH,
     ReportType("globalprotect_vuln"): Severity.HIGH,
     ReportType("lfi_vulnerability"): Severity.HIGH,
     ReportType("rce_vulnerability"): Severity.HIGH,

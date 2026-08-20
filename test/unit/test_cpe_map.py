551fff0d6e6ed24d53862607d5ae51a9b1efca45
Krzysztof Zając <krzysztof.zajac@cert.pl>
2026-08-20 12:46:57 +0200

    .

--- test/unit/test_cpe_map.py
+++ test/unit/test_cpe_map.py
@@ -1,6 +1,7 @@
+import unittest
+
 from artemis.reporting.modules.wordpress_plugins.cpe_map import download_cpe_map
 
-import unittest
 
 class TestDownloadCPEMap(unittest.TestCase):
     def test_download_cpe_map(self) -> None:

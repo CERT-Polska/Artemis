fd164cbfedfd581db21ee6473dd9ef9bf8abcbcc
Krzysztof Zając <krzysztof.zajac@cert.pl>
2026-08-28 23:03:13 +0200

    .

--- artemis/cpe_tools/cpe_main_process.py
+++ artemis/cpe_tools/cpe_main_process.py
@@ -97,7 +97,11 @@ def _iter_entries(chunks_dir: Path) -> Iterator[tuple[str, str, list[str]]]:
                 continue
             title = None
             for entry in cpe.get("titles", []):
-                if isinstance(entry, dict) and entry.get("lang") is not None and entry.get("lang").lower().startswith("en"):
+                if (
+                    isinstance(entry, dict)
+                    and entry.get("lang") is not None
+                    and entry.get("lang").lower().startswith("en")  # type: ignore
+                ):
                     title = entry.get(TITLE)
                     break
             if not title:

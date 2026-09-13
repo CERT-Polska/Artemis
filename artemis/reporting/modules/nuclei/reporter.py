414b5be8075c836c68b332b60391125892376c11
Krzysztof Zając <krzysztof.zajac@cert.pl>
2026-09-13 20:48:35 +0200

    .

--- artemis/reporting/modules/nuclei/reporter.py
+++ artemis/reporting/modules/nuclei/reporter.py
@@ -100,7 +100,7 @@ def extract_request_target(host: str, request: str | None) -> tuple[str, str] | 
 
     try:
         command, target = request.splitlines()[0].split(" ", 1)
-        assert command == "curl"
+        assert command == "curl", f"{command} is not 'curl'"
         assert target.startswith("http://") or target.startswith("https://")
     except IndexError:
         return None

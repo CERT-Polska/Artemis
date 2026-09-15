9ef33f38a2876b3bd331b7b1eff74e33efd8cad9
Krzysztof Zając <krzysztof.zajac@cert.pl>
2026-09-15 15:26:33 +0200

    .

--- test/reporting/test_nuclei_autoreporter_integration.py
+++ test/reporting/test_nuclei_autoreporter_integration.py
@@ -120,4 +120,7 @@ class NucleiAutoreporterIntegrationTest(BaseReportingTest):
             self.assertEqual(pqf_parsed.query, matched_at_parsed.query)
 
         message = self.task_result_to_message(data, custom_template_arguments={"always_show_full_poc": True})
-        self.assertIn("http://test-redirect-reflect-app:5000 under /?uname=%27%3E%22%3Csvg", " ".join(message.split()))
+        self.assertRegex(
+            " ".join(message.split()),
+            r"http://test-redirect-reflect-app:5000 under /\?\w+=%27%3E%22%3Csvg",
+        )

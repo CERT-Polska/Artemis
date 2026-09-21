6e5c62effbfc897fe62a90ce72e2b2ccf0365461
kazet <krzysztof.zajac@cert.pl>
2026-09-21 17:22:00 +0200

    More robust request extraction,Nuclei translation (#3159)
    * More robust request extraction
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * Revert "."
    
    This reverts commit 74f134e069fee36951101231bbb81f74917aea95.
    
    * /
    
    * .
    
    * .
    
    * Apply suggestion from @kazet

--- artemis/reporting/modules/nuclei/reporter.py
+++ artemis/reporting/modules/nuclei/reporter.py
@@ -26,9 +26,12 @@ from artemis.reporting.utils import (
     get_target_url,
     get_top_level_target,
 )
-from artemis.utils import get_host_from_url
+from artemis.utils import build_logger, get_host_from_url
 
 from .translations.nuclei_messages import pl_PL as translations_nuclei_messages_pl_PL
+
+logger = build_logger(__name__)
+
 
 TECHNOLOGY_NAME_MAPPINGS = {
     "Apache": "Apache HTTP Server",
@@ -106,12 +109,19 @@ def extract_request_target(host: str, request: str | None) -> tuple[str, str] | 
 
     try:
         method, target, protocol = request.splitlines()[0].split(" ", 3)
-    except IndexError:
+    except (IndexError, ValueError):
+        logger.error("Unable to extract request URL for: %s", request)
+        return None
+
+    if target.startswith("sip:"):
         return None
 
     assert method in [method.value for method in HTTPMethod], f"{method} is not a standard HTTP verb"
     assert (
-        target.startswith("http://") or target.startswith("https://") or target.startswith("/")
+        target.startswith("http://")
+        or target.startswith("https://")
+        or target.startswith("/")
+        or target.startswith("./")
     ), f"{target} should start with proto or /"
     assert protocol.startswith("HTTP/"), f"{protocol} should start with HTTP/"
 

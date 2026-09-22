922f0331806f5ed0c04b4bbb957200418e50bd69
kazet <krzysztof.zajac@cert.pl>
2026-09-22 09:59:47 +0200

    WordPress plugin identification fix (#3174)

--- artemis/modules/wordpress_plugins.py
+++ artemis/modules/wordpress_plugins.py
@@ -72,6 +72,7 @@ PLUGINS_BAD_VERSION_IN_README = [
     "link-manager",
     "login-logo",
     "page-or-post-clone",
+    "persian-woocommerce-shipping",
     "search-meter",
     "website-monetization-by-magenet",
     "wp-maximum-execution-time-exceeded",

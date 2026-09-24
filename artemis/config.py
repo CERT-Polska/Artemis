a9f9d40d9bce5b58c743e8fb7e85dfdf9a3f4306
kazet <krzysztof.zajac@cert.pl>
2026-09-24 18:34:59 +0200

    One more blocklist (#3177)

--- artemis/config.py
+++ artemis/config.py
@@ -565,6 +565,7 @@ class Config:
                     [
                         # We already have Wappalyzer
                         "http/technologies/tech-detect.yaml",
+                        "http/technologies/fingerprinthub-web-fingerprints.yaml",
                         # Too many FP
                         "http/technologies/waf-detect.yaml",
                         # wordpress plugins detections, we have separate module
